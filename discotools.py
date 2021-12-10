57a8edce998e0c8b566d4023a7ea038b092f71eb
Enegg <42005170+Enegg@users.noreply.github.com>
2021-12-10 03:05:41 +0100

    removal of obsolete functions/classes added channel logging classes

--- discotools.py
+++ discotools.py
@@ -2,285 +2,98 @@
 from __future__ import annotations
 
 import asyncio
+import io
+import logging
 import re
+import traceback
+from collections import deque
 from functools import partial
 from typing import *
 
 import disnake
 from disnake.ext import commands
 
-VT = TypeVar("VT")
-C = TypeVar('C', bound=commands.Command)
-Predicate = Callable[..., bool]
+if TYPE_CHECKING:
+    from types import TracebackType
 
-# decorators
-def perms(lvl: int):
-    """Defines required user's lvl to access a command, following:
-    1 - manage messages, 2 - manage guild, 3 - admin, 4 - guild owner, 5 - bot author"""
-    async def extended_check(ctx: commands.Context[commands.Bot]) -> bool:
-        if await ctx.bot.is_owner(ctx.author):
-            return True
+T = TypeVar('T')
+AnyContext = Union[commands.Context, disnake.ApplicationCommandInteraction]
 
-        if ctx.guild is None:
-            return False
+def channel_lock(
+    words: Iterable[str]=None,
+    regex: str | re.Pattern[str]=None,
+    admins_bypass: bool=True,
+    dm_channels_pass: bool=True
+) -> Callable[[T], T]:
+    """Locks command from use in channels that are not "spam" or "bot" channels.
+    It's an error to specify both words and regex.
 
-        if lvl <= 4 and ctx.guild.owner_id == ctx.author.id:
-            return True
-
-        perm_keys = ('manage_messages', 'manage_guild', 'administrator')
-
-        if lvl <= len(perm_keys):
-            key = perm_keys[lvl - 1]
-            assert isinstance(ctx.author, disnake.Member)
-            assert isinstance(ctx.channel, (disnake.TextChannel, disnake.Thread))
-            return getattr(ctx.channel.permissions_for(ctx.author), key, False)
-
-        return False
-
-    return commands.check(extended_check)
-
-
-def spam_command(rate: int=None, per: float=None, bucket: commands.BucketType=commands.BucketType.default, *, ignore_for_admins: bool=True, regex: str=None):
-    """Ratelimits the command in channels that are not "spam" or "bot" channels.
-    If rate argument is not passed, the command is disabled for those channels entirely."""
-    if rate is per is None:
-        cd = False
-
-    elif rate is None or per is None:
-        raise TypeError('Specified one of two cooldown arguments; both are required')
-
-    else:
-        cd = True
-
-    def base_check(ctx: commands.Context) -> bool:
-        channel = ctx.channel
-
-        assert ctx.command is not None
-
-        if isinstance(channel, disnake.DMChannel):
-            ctx.command.reset_cooldown(ctx)
-            return True
-
-        assert isinstance(channel, disnake.TextChannel)
-        assert isinstance(ctx.author, disnake.Member)
-
-        if ignore_for_admins and ctx.author.guild_permissions.administrator:
-            ctx.command.reset_cooldown(ctx)
-            return True
-
-        return False
-
+    Parameters:
+    ------------
+    words:
+        Iterable of words a channel name should have one of to be considered "spam" channel.
+        Defaults to ['bot', 'spam']
+    regex:
+        A regex to match channel name with.
+    admins_bypass:
+        Whether to passthrough members with administrator privilege.
+    dm_channels_pass:
+        Whether to allow the command in DM channels.
+    """
     if regex is not None:
+        if words is not None:
+            raise TypeError('Using both words and regex is not allowed')
+
         search = partial(re.search, pattern=re.compile(regex))
 
-        def check_re(ctx: commands.Context) -> bool:
-            if base_check(ctx):
-                return True
-
-            assert ctx.command is not None
-            assert isinstance(ctx.channel, (disnake.TextChannel, disnake.Thread))
-
-            if search(ctx.channel.name.lower()) is not None:
-                ctx.command.reset_cooldown(ctx)
-                return True
-
-            return cd
-
-        check = check_re
-
     else:
-        def check_str(ctx: commands.Context) -> bool:
-            if base_check(ctx):
-                return True
-
-            assert ctx.command is not None
-            assert isinstance(ctx.channel, (disnake.TextChannel, disnake.Thread))
-
-            name = ctx.channel.name.lower()
-
-            if any(s in name for s in {'spam', 'bot'}):
-                ctx.command.reset_cooldown(ctx)
-                return True
-
-            return cd
-
-        check = check_str
-
-
-    if cd:
-        assert rate is not None and per is not None
-
-        def cd_wrapper(command: C) -> C:
-            command = commands.cooldown(rate, per, bucket)(command)
-            command.cooldown_after_parsing = True
-
-            return commands.check(check)(command)
-
-        return cd_wrapper
-
-    else:
-        return commands.check(check)
-
-
-# embed customization
-class EmbedUI(disnake.Embed):
-    """Preset for an embed creating a choice menu
-
-    By default, it holds these emojis:
-    0️⃣1️⃣2️⃣3️⃣4️⃣5️⃣6️⃣7️⃣8️⃣9️⃣🔟
-    """
-
-    NUMBERS = ('0️⃣', '1️⃣', '2️⃣', '3️⃣', '4️⃣', '5️⃣', '6️⃣', '7️⃣', '8️⃣', '9️⃣', '🔟')
-
-    def __init__(self, emojis: list[str] | None=None, msg: disnake.Message | commands.Context | None=None, **kwargs: Any):
-        if desc := kwargs.get('desc'):
-            kwargs.setdefault('description', desc)
-            del kwargs['desc']
-
-        super().__init__(**kwargs)
-        self._emojis = emojis or list(self.NUMBERS)
-        self._count = len(self.emojis)
-
-        if isinstance(msg, commands.Context):
-            self._msg = msg.message
-
-        elif msg is None or isinstance(msg, disnake.Message):
-            self._msg = msg
+        if words is None:
+            _words = frozenset(('bot', 'spam'))
 
         else:
-            raise TypeError('msg argument must be Message, Context or None')
+            _words = frozenset(words)
+
+        search = lambda name: any(s in name for s in _words)
+
+    def channel_check(inter: AnyContext) -> bool:
+        channel = inter.channel
+
+        if isinstance(channel, disnake.DMChannel):
+            if not dm_channels_pass:
+                raise commands.NoPrivateMessage()
+
+            return True
+
+        assert isinstance(inter.author, disnake.Member)
+
+        if admins_bypass and inter.author.guild_permissions.administrator:
+            return True
+
+        if search(channel.name.lower()):
+            return True
+
+        raise ForbiddenChannel()
+
+    return commands.check(channel_check)
 
 
-    def set_default(self, ctx: commands.Context):
-        """Sets embed author"""
-        self.set_author(name=f'Requested by {ctx.author.display_name}', icon_url=ctx.author.display_avatar)
-        return self
-
-
-    @property
-    def count(self):
-        return self._count
-
-    @count.setter
-    def count(self, value: int):
-        if value > len(self.emojis):
-            raise ValueError('Value greater than emoji count')
-
-        self._count = value
-
-
-    @property
-    def emojis(self):
-        return self._emojis
-
-    @emojis.setter
-    def emojis(self, items: list[str]):
-        self._emojis = items
-        self.count = len(items)
-
-
-    @property
-    def msg(self):
-        if self._msg is None:
-            raise AttributeError('Message not passed to embed')
-
-        return self._msg
-
-    @msg.setter
-    def msg(self, value: disnake.Message | commands.Context):
-        if not isinstance(value, (disnake.Message, commands.Context)):
-            raise TypeError(f'Expected Context or Message object, got {type(value)}')
-
-        if isinstance(value, commands.Context):
-            value = value.message
-
-        self._msg = value
-
-
-    async def add_options(self, add_cancel: bool=False):
-        """Reacts to a message with emojis as options, returns a list of emojis"""
-        reactions = self.emojis[:self.count]
-
-        if add_cancel:
-            reactions.append('❌')
-
-        for x in reactions:
-            await self.msg.add_reaction(x)
-
-        return reactions
-
-
-    async def edit(self, add_return: bool=False):
-        """Edits the message"""
-        await self.msg.edit(embed=self)
-
-        if add_return:
-            await self.msg.add_reaction('↩️')
-
-        return self
-
-
-
-def make_choice_embed(ctx: commands.Context, items: Sequence[VT], *, name_getter: Callable[[VT], str]=str, **kwargs: Any) -> disnake.Embed:
-    """Given 1 < len(items), enumerates items and puts them in embed's body.
-    Embed can be customized from kwargs, those are passed to embed constructor.
-    'title' has {number} argument passed for formatting.
-    'description_list' has {number}, {padding} & {item} passed for formatting.
-
-    Defaults:
-
-    title='Found {n} items!'
-
-    desc=description='Type a number to get the item\\n'
-
-    desc_item='**{n:{p}}**. **{i}**'
-    """
-    number = len(items)
-
-    if number < 2:
-        raise ValueError('Sequence contains fewer than 2 items')
-
-    padding = len(str(number)) + 1
-    kwargs['title']  = kwargs.get('title', 'Found {n} items!').format(number=number, n=number)
-    desc = kwargs.get('description', kwargs.get('desc', 'Type a number to get the item\n'))
-    fill = kwargs.get('desc_item', '**{n:{p}}**. **{i}**')
-    desc += '\n'.join(fill.format(number=n, item=i, padding=padding, n=n, i=i, p=padding) for n, i in enumerate(map(name_getter, items), 1))
-    kwargs['description'] = desc
-    kwargs['color']  = kwargs.get('color', kwargs.get('colour', ctx.author.color.value))
-    kwargs['author'] = kwargs.get('author', {'name': f'Requested by {ctx.author.display_name}', 'icon_url': ctx.author.display_avatar.url})
-
-    embed = disnake.Embed.from_dict(kwargs)
-
-    if len(embed) > 6000:
-        raise ValueError('Embed body exceeded 6000 character limit')
-
-    return embed
-
-
-async def get_message(ctx: commands.Context[commands.Bot], predicate: Callable[[disnake.Message], bool]=None, timeout: float=None) -> disnake.Message:
-    """Awaits a message from Context's author that meets the predicate (default implementation checks author, channel and content.isdigit())
-    and returns it. Raises TimeoutError on timeout and ValueError if the input was invalid."""
-
-    if predicate is None:
-        def check(m: disnake.Message):
-            return m.author == ctx.author and m.channel == ctx.channel and m.content.isdigit()
-
-        predicate = check
-
-    return await ctx.bot.wait_for('message', check=predicate, timeout=timeout)
-
-
-async def scheduler(ctx: commands.Context, events: Iterable[str], check: Predicate | Iterable[Predicate], timeout: float = None,
-                    return_when: Literal['FIRST_COMPLETED', 'FIRST_EXCEPTION', 'ALL_COMPLETED'] = 'FIRST_COMPLETED') -> set[tuple[Any, str]]:
-    """Wrapper for asyncio.wait designed for discord events. Returns the outcome of the event and its name."""
-    if not events:
-        raise ValueError('No events to await')
-
+async def scheduler(
+    client: disnake.Client,
+    events: Iterable[str],
+    check: Callable[..., bool] | Iterable[Callable[..., bool]],
+    timeout: float = None,
+    return_when: Literal['FIRST_COMPLETED', 'FIRST_EXCEPTION', 'ALL_COMPLETED'] = 'FIRST_COMPLETED'
+) -> set[tuple[Any, str]]:
+    """Wrapper for `Client.wait_for` accepting multiple events. Returns the outcome of the event and its name."""
     if isinstance(check, Iterable):
-        tasks = {asyncio.create_task(ctx.bot.wait_for(event, check=_check), name=event) for event, _check in zip(events, check)}
+        tasks = {asyncio.create_task(client.wait_for(event, check=_check), name=event) for event, _check in zip(events, check)}
 
     else:
-        tasks = {asyncio.create_task(ctx.bot.wait_for(event, check=check), name=event) for event in events}
+        tasks = {asyncio.create_task(client.wait_for(event, check=check), name=event) for event in events}
+
+    if not tasks:
+        raise ValueError('No events to await')
+
 
     done, pending = await asyncio.wait(tasks, timeout=timeout, return_when=return_when)
 
@@ -293,57 +106,49 @@ async def scheduler(ctx: commands.Context, events: Iterable[str], check: Predica
     return {(await task, task.get_name()) for task in done}
 
 
-
-class Flag(commands.Converter):
-    """Converter for CLI style flags
-
-    Usage: command_name(ctx, arg: Flag['-foo', '-bar'])
-
-    Returns flags found in command arguments
-    """
-    flags: tuple[str, ...] | None
-
-    def __new__(cls, *, flags: tuple[str, ...]=None):
-        self = super(Flag, cls).__new__(cls)
-        self.flags = flags
-        return self
+class DiscordFormatter(logging.Formatter):
+    def formatException(self, exc_info: tuple[type[BaseException] | None, BaseException | None, TracebackType | None]) -> str:
+        with io.StringIO() as sio:
+            sio.write('```\n')
+            traceback.print_exception(*exc_info, file=sio)
+            sio.write('```')
+            return sio.getvalue()
 
 
-    def __class_getitem__(cls, flags: str | tuple[str, ...]):
-        if isinstance(flags, str):
-            flags = (flags,)
-
-        elif not isinstance(flags, tuple) or any(not isinstance(i, str) for i in flags):
-            raise TypeError(f'{type(cls).__name__}[...] only accepts literal strings')
-
-        return cls(flags=flags)
-
-
-    def __contains__(self, key: str) -> bool:
-        return self.flags is not None and key in self.flags
+class ChannelHandler(logging.Handler):
+    def __init__(self, channel_id: int, client: disnake.Client, level: int=logging.NOTSET) -> None:
+        super().__init__(level)
+        self.client = client
+        self.dest: disnake.abc.Messageable | None = None
+        self.queue = deque[logging.LogRecord]()
+        self.formatter = DiscordFormatter()
 
 
-    def __repr__(self):
-        return f"{type(self).__name__}{'[' + ', '.join(self.flags) + ']' if self.flags else ''}"
+        def setter(future: asyncio.Future[Any]) -> None:
+            channel = client.get_channel(channel_id)
+
+            if not isinstance(channel, disnake.abc.Messageable):
+                raise TypeError('Channel is not Messengable')
+
+            self.dest = channel
+
+            while self.queue:
+                self.emit(self.queue.popleft())
+
+        client.loop.create_task(client.wait_until_ready()).add_done_callback(setter)
 
 
-    async def convert(self, ctx: commands.Context, arg: str) -> str:
-        if self.flags is None:
-            raise AttributeError('Converter used without specifying flags')
+    def emit(self, record: logging.LogRecord) -> None:
+        if self.dest is None:
+            self.queue.append(record)
+            return
 
-        arg = arg.lower()
-
-        for flag in self.flags:
-            if flag.startswith(arg):
-                return flag
-
-        else:
-            raise commands.BadArgument
+        msg = self.format(record)
+        self.client.loop.create_task(self.dest.send(msg, allowed_mentions=disnake.AllowedMentions.none()))
 
 
+class ForbiddenChannel(commands.CheckFailure):
+    """Exception raised when command is used from invalid channel."""
 
-def async_from_sync(loop: asyncio.AbstractEventLoop, coro: Awaitable[Any], callback: Callable[[asyncio.Task[Any]], Any]=None):
-    task = loop.create_task(coro)
-
-    if callback is not None:
-        task.add_done_callback(callback)
+    def __init__(self, message: Optional[str] = None, *args: Any) -> None:
+        super().__init__(message=message or 'You cannot use this command here.', *args)
--- functions.py
+++ functions.py
@@ -1,6 +1,9 @@
 """Functions that can find use outside of discord.py scope"""
 from __future__ import annotations
 
+import random
+from collections import deque
+from string import ascii_letters
 from typing import *
 
 SupportsSet = TypeVar('SupportsSet', bound=Hashable)
@@ -19,19 +22,30 @@ def common_items(*items: Iterable[SupportsSet]) -> set[SupportsSet]:
     return result
 
 
-def search_for(phrase: str, iterable: Iterable[str]) -> Iterator[str]:
-    """Helper func capable of finding a specific string following a name rule,
-    like `hal burn sco` in `Half Burnt Scope`"""
-    parts = phrase.lower().split()
+def search_for(phrase: str, iterable: Iterable[str], *, case_sensitive: bool=False) -> Iterator[str]:
+    """Helper func capable of finding a specific string(s) in iterable.
+    It is considered a match if every word in phrase appears in the name
+    and in the same order. For example, both `burn scop` & `half scop`
+    would match name `Half Burn Scope`, but not `burn half scop`.
+
+    Parameters
+    -----------
+    phrase:
+        String of whitespace-separated words.
+    iterable:
+        Iterable of strings to match against.
+    case_sensitive:
+        Whether the search should be case sensitive."""
+    parts = (phrase if case_sensitive else phrase.lower()).split()
 
     for name in iterable:
-        words = iter(name.lower().split())
+        words = iter((name if case_sensitive else name.lower()).split())
 
         if all(any(word.startswith(prefix) for word in words) for prefix in parts):
             yield name
 
 
-def split_to_fields(all_items: list[AnyStr], offset: int=1, field_limit: int | tuple[int, int]=2048) -> list[list[AnyStr]]:
+def split_to_fields(all_items: Iterable[AnyStr], offset: int=1, field_limit: int | tuple[int, int]=2048) -> list[list[AnyStr]]:
     """Splits a list of strings into multiple lists (to-be embed fields) so that they stay under character limit.
     Field_limit should be an int or a tuple of two ints;
     in the latter case the first int will be applied to the first field, and the second to any following field."""
@@ -47,29 +61,23 @@ def split_to_fields(all_items: list[AnyStr], offset: int=1, field_limit: int | t
     else:
         main_limit, extra_limit = field_limit, 0
 
-    slices_list: list[list[AnyStr]] = []
-    last = 0
+    current_field: list[AnyStr] = []
+    fields_list: list[list[AnyStr]] = [current_field]
     sub_len = 0
 
-    for i, item in enumerate(all_items):
-        if slices_list and extra_limit:
+    for item in all_items:
+        if fields_list and extra_limit:
             main_limit = extra_limit
 
         if sub_len + len(item) > main_limit:
-            slices_list.append(all_items[last:i])
-            sub_len, last = 0, i
+            current_field = []
+            fields_list.append(current_field)
+            sub_len = 0
 
+        current_field.append(item)
         sub_len += len(item) + offset
 
-    else:
-        slices_list.append(all_items[last:])
-
-    return slices_list
-
-
-def filter_flags(flags: Iterable[SupportsSet], items: Iterable[SupportsSet]) -> tuple[frozenset[SupportsSet], Iterator[SupportsSet]]:
-    _flags = frozenset(flags).intersection(items)
-    return _flags, (s for s in items if s not in _flags)
+    return fields_list
 
 
 def js_format(string: str, **kwargs: Any) -> str:
@@ -77,3 +85,100 @@ def js_format(string: str, **kwargs: Any) -> str:
         string = string.replace(f'%{key}%', str(value))
 
     return string
+
+
+def random_str(length: int) -> str:
+    """Generates a random string of given length from ascii letters"""
+    return ''.join(random.sample(ascii_letters, length))
+
+
+class StringIterator(Iterator[str]):
+    def __init__(self, string: str) -> None:
+        self._unread: deque[str] = deque()
+        self.iter = iter(string)
+        self.last: str | None = None
+
+    def __next__(self) -> str:
+        if self._unread:
+            self.last = self._unread.popleft()
+
+        else:
+            self.last = next(self.iter)
+
+        return self.last
+
+    def unread(self) -> None:
+        if self.last is None:
+            return
+
+        self._unread.appendleft(self.last)
+        self.last = None
+
+
+def parse_kwargs(args: Iterable[str]):
+    """Takes command arguments as an input and tries to match them as key item pairs"""
+    # keyword <whitespace> (optional operator: ":" "=" "!=" "≠" "<=" "≤" ">=" "≥" "<" ">") <whitespace> (optional value), ...
+    # keyword: TYPE, ELEMENT, TIER, NAME, STAT
+
+    # start with finding operator - they restrict possible keywords, so it narrows searching
+    chars = {'=', '<', '>', '!', ':', '≠', '≤', '≥'}
+
+    parsed_args = []
+
+    for arg in args:
+        keyword = operator = value = ''
+        str_parts = ['']
+
+        space = False
+        it = StringIterator(arg)
+
+        for c in it:
+            if c == ' ':
+                space = True
+                continue
+
+            if c in chars:
+                if not keyword:
+                    keyword = ' '.join(str_parts)
+                    str_parts = ['']
+
+                if operator and operator != ':':
+                    raise ValueError('Second operator found')
+
+                n = next(it, None)
+
+                if n is None:
+                    raise ValueError('End of string while parsing operator')
+
+                if n == '=':
+                    try:
+                        # !=, ==, <=, >=
+                        # c = concat_op(c)
+                        pass
+
+                    except KeyError:
+                        raise ValueError(f'Invalid operator: "{c}="')
+
+                else:
+                    it.unread()
+
+                operator = c
+                space = False  # whatever the value, set to False
+                continue
+
+            # either keyword or value
+            if space:
+                str_parts.append('')
+                space = False
+
+            str_parts[-1] += c
+
+        if not keyword:
+            keyword = ' '.join(str_parts).strip()
+
+        else:
+            value = ' '.join(str_parts).strip()
+
+        parsed_args.append((keyword, operator, value))
+
+    return parsed_args
