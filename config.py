dc390ced1c94e762386336d6a9bf6a38d431fbab
Enegg <42005170+Enegg@users.noreply.github.com>
2021-12-10 03:06:06 +0100

    removed obsolete config vars

--- config.py
+++ config.py
@@ -1,12 +1,7 @@
 """Config file containing basic variables."""
 #general vars
-PREFIX_LOCAL = ";"
-PREFIX_HOST = 's!'
 LOGS_CHANNEL = 801620062821023744
 NONE_EMOJI = '<:none:772958360240128060>'
 #SM
 ITEMS_JSON_LINK = 'https://raw.githubusercontent.com/Enegg/Item-packs/master/items.json'
 DEFAULT_PACK_URL = 'https://gist.githubusercontent.com/ctrl-raul/3b5669e4246bc2d7dc669d484db89062/raw'
-IMAGE_LINK_TEMPLATES = ('https://raw.githubusercontent.com/ctrl-raul/supermechs-item-images/master/png/{}.png',
-                        'https://raw.githubusercontent.com/ctrl-raul/workshop-unlimited/master/items/{}.png',
-                        'https://raw.githubusercontent.com/Lookotza/smBot/master/items/{}.png')
