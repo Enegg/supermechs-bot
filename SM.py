20543cbce090da1708f8ee4d36955e420ab52615
Enegg <42005170+Enegg@users.noreply.github.com>
2022-01-05 02:16:22 +0100

    Style changes Adjustments for use with odmantic models autopep8

--- SM.py
+++ SM.py
@@ -9,17 +9,17 @@ import disnake
 from disnake.ext import commands
 
 from config import DEFAULT_PACK_URL, TEST_GUILDS
-from functions import random_str, search_for
-from image_manipulation import image_to_file
-from SM_classes import (STAT_NAMES, AnyAttachment, ArenaBuffs, Item, ItemPack,
-                        Player, Rarity)
+from images import image_to_file
+from SM_classes import STAT_NAMES, AnyItem, ArenaBuffs, Item, ItemPack, Player, Rarity
 from ui_components import ArenaBuffsView, ItemView, MechView
+from utils import random_str, search_for
 
 if TYPE_CHECKING:
-    from UltraMech import HostedBot
+    from UltraMech import SMBot
 
 # helper functions for ,stats
 MAX_BUFFS = ArenaBuffs.maxed()
+
 
 def buff(stat: str, enabled: bool, value: int) -> int:
     """Returns a value buffed respectively to stat type"""
@@ -37,7 +37,7 @@ def buff_difference(stat: str, enabled: bool, value: int) -> tuple[int, int]:
     return MAX_BUFFS.total_buff_difference(stat, value)
 
 
-def default_embed(embed: disnake.Embed, item: Item[AnyAttachment], buffs_enabled: bool) -> None:
+def default_embed(embed: disnake.Embed, item: AnyItem, buffs_enabled: bool) -> None:
     rarity = item.rarity
 
     if isinstance(rarity, Rarity):
@@ -93,7 +93,7 @@ def default_embed(embed: disnake.Embed, item: Item[AnyAttachment], buffs_enabled
     embed.add_field(name='Stats:', value=item_stats, inline=False)
 
 
-def compact_embed(embed: disnake.Embed, item: Item[AnyAttachment], buffs_enabled: bool) -> None:
+def compact_embed(embed: disnake.Embed, item: AnyItem, buffs_enabled: bool) -> None:
     rarity = item.rarity
 
     if isinstance(rarity, Rarity):
@@ -130,7 +130,7 @@ def compact_embed(embed: disnake.Embed, item: Item[AnyAttachment], buffs_enabled
     if line_count > 4 and not 0 != line_count % 4 < 3:  # == 0 or > 2
         div = 4
 
-    elif not 0 != line_count % 3 < 2: # == 0 or > 1
+    elif not 0 != line_count % 3 < 2:  # == 0 or > 1
         div = 3
 
     elif line_count < 4:
@@ -148,26 +148,24 @@ def compact_embed(embed: disnake.Embed, item: Item[AnyAttachment], buffs_enabled
 
 class SuperMechs(commands.Cog):
     """Set of commands related to the SuperMechs game."""
-    items_dict: dict[str, Item[AnyAttachment]]
+    items_dict: dict[str, AnyItem]
     abbrevs: dict[str, set[str]]
 
-    def __init__(self, bot: HostedBot) -> None:
+    def __init__(self, bot: SMBot) -> None:
         self.bot = bot
         self.image_url_cache: dict[str, str] = {}
         self.no_img:   set[str] = set()
         self.no_stats: set[str] = set()
         self.players: dict[int, Player] = {}  # TODO: replace with actual database
 
-
     @property
     def session(self) -> aiohttp.ClientSession:
         return self.bot.session
 
-
     async def cog_load(self) -> None:
         await self.load_item_pack(DEFAULT_PACK_URL)
         self.abbrevs = self.abbreviate_names(self.items_dict)
-
+        Item.loader = lambda name: self.items_dict[name]
 
     async def load_item_pack(self, pack_url: str, /) -> None:
         """Loads an item pack from url and sets it as active pack."""
@@ -175,9 +173,8 @@ class SuperMechs(commands.Cog):
             pack: ItemPack = await response.json(encoding='utf-8', content_type=None)
 
         self.items_dict = {
-            item_dict['name']: Item(**item_dict, pack=pack['config'])
+            item_dict['name']: Item(**item_dict, pack=pack['config'])  # type: ignore[assignment]
             for item_dict in pack['items']}
-
 
     @staticmethod
     def abbreviate_names(names: Iterable[str], /) -> dict[str, set[str]]:
@@ -197,9 +194,9 @@ class SuperMechs(commands.Cog):
             abbrev = {''.join(a for a in name if a.isupper()).lower()}
 
             if not is_single_word:
-                abbrev.add(name.replace(' ', '').lower()) # Fire Fly => firefly
+                abbrev.add(name.replace(' ', '').lower())  # Fire Fly => firefly
 
-            if not IsNotPascal and is_single_word: # takes care of PascalCase names
+            if not IsNotPascal and is_single_word:  # takes care of PascalCase names
                 last = 0
                 for i, a in enumerate(name):
                     if a.isupper():
@@ -215,12 +212,23 @@ class SuperMechs(commands.Cog):
 
         return abbrevs
 
+    def get_player(self, data: disnake.User | disnake.Member | commands.Context | disnake.Interaction | int, /) -> Player:
+        """Return a Player object from object containing user ID."""
+        match data:
+            case disnake.User() | disnake.Member():
+                id = data.id
 
-    def get_player(self, inter: disnake.ApplicationCommandInteraction, /) -> Player:
-        id = inter.author.id
+            case commands.Context() | disnake.Interaction():
+                id = data.author.id
+
+            case int():
+                id = data
+
+            case _:
+                raise TypeError(f'Invalid type: {type(data)}')
 
         if id not in self.players:
-            self.players[id] = Player(id)
+            self.players[id] = Player(id=id)
 
         return self.players[id]
 
@@ -238,7 +246,6 @@ class SuperMechs(commands.Cog):
         await self.load_item_pack(url)
         await inter.send('Success', ephemeral=True)
 
-
     @commands.slash_command()
     async def frantic(self, inter: disnake.ApplicationCommandInteraction) -> None:
         """Humiliate frantic users"""
@@ -246,16 +253,14 @@ class SuperMechs(commands.Cog):
         choice = random.choice(frantics)
         await inter.send(choice)
 
-
     @commands.slash_command()
-    # @channel_lock(['bot', 'spam', 'supermechs', 'command', 'playground'])
     async def item(
         self,
         inter: disnake.ApplicationCommandInteraction,
         name: str,
-        compact: bool=False,
-        invisible: bool=True,
-        raw: bool=False
+        compact: bool = False,
+        invisible: bool = True,
+        raw: bool = False
     ) -> None:
         """Finds an item and returns its stats
 
@@ -282,32 +287,31 @@ class SuperMechs(commands.Cog):
             return
 
         if compact:
-            embed = disnake.Embed(color=item.element.color
-            ).set_author(name=item.name, icon_url=item.icon.URL
-            ).set_thumbnail(url=item.image_url)
+            embed = (disnake.Embed(
+                color=item.element.color)
+                .set_author(name=item.name, icon_url=item.icon.URL)
+                .set_thumbnail(url=item.image_url))
             view = ItemView(embed, item, compact_embed)
 
         else:
-            embed = disnake.Embed(title=item.name,
+            embed = (disnake.Embed(
+                title=item.name,
                 description=f"{item.element.name.capitalize()} {item.type.replace('_', ' ').lower()}",
-                color=item.element.color
-            ).set_thumbnail(url=item.icon.URL
-            ).set_image(url=item.image_url)
+                color=item.element.color)
+                .set_thumbnail(url=item.icon.URL)
+                .set_image(url=item.image_url))
             view = ItemView(embed, item, default_embed)
 
         await inter.send(embed=embed, view=view, ephemeral=invisible)
         await view.wait()
         await inter.edit_original_message(view=None)
 
-
     @commands.slash_command()
-    # @channel_lock(['bot', 'spam', 'supermechs', 'command', 'playground'])
     async def mech(self, inter: disnake.ApplicationCommandInteraction) -> None:
         pass
 
-
     @mech.sub_command()
-    async def show(self, inter: disnake.ApplicationCommandInteraction, name: str=None) -> None:
+    async def show(self, inter: disnake.ApplicationCommandInteraction, name: str = None) -> None:
         """Displays your mech and its stats
 
         Parameters
@@ -318,13 +322,13 @@ class SuperMechs(commands.Cog):
         player = self.get_player(inter)
 
         if name is None:
-            mech = player.get_active_build()
+            mech = player.active_build
 
             if mech is None:
-                await inter.send("You do not have any builds.")
+                await inter.send('You do not have any builds.', ephemeral=True)
                 return
 
-            name = player.active_build
+            name = player.active_build_name
 
         elif name in player.builds:
             mech = player.builds[name]
@@ -351,17 +355,16 @@ class SuperMechs(commands.Cog):
         file = image_to_file(mech.image, filename)
         await inter.send(embed=embed, file=file)
 
-
     @mech.sub_command(name='list')
     async def browse(self, inter: disnake.ApplicationCommandInteraction) -> None:
         """Displays a list of your builds"""
         player = self.get_player(inter)
 
         if not player.builds:
-            await inter.send('You do not have any builds.')
+            await inter.send('You do not have any builds.', ephemeral=True)
             return
 
-        string = f'Currently active: **{player.active_build}**\n'
+        string = f'Currently active: **{player.active_build_name}**\n'
 
         string += '\n\n'.join(
             f'**{name}**:\n'
@@ -374,10 +377,9 @@ class SuperMechs(commands.Cog):
 
         await inter.send(string)
 
-
     @mech.sub_command()
     @commands.max_concurrency(1, commands.BucketType.user)
-    async def build(self, inter: disnake.ApplicationCommandInteraction, name: str=None) -> None:
+    async def build(self, inter: disnake.ApplicationCommandInteraction, name: str = None) -> None:
         """Interactive UI for modifying a mech build.
 
         Parameters
@@ -390,7 +392,7 @@ class SuperMechs(commands.Cog):
 
         if name is None:
             mech = player.get_or_create_build()
-            name = player.active_build
+            name = player.active_build_name
 
         elif name not in player.builds:
             mech = player.new_build(name)
@@ -401,12 +403,7 @@ class SuperMechs(commands.Cog):
         embed = disnake.Embed(title=f'Mech build "{name}"', color=inter.author.color)
         embed.add_field(name='Stats:', value=mech.print_stats())
 
-        view = MechView(mech, embed, self.items_dict, player.arena_buffs, self.session)
-
-        async def on_timeout() -> None:
-            await inter.edit_original_message(view=None)
-
-        view.on_timeout = on_timeout
+        view = MechView(mech, embed, self.items_dict, player.arena_buffs, self.session, user_id=inter.author.id, timeout=100)
 
         if mech.torso is None:
             await inter.send(embed=embed, view=view)
@@ -422,6 +419,10 @@ class SuperMechs(commands.Cog):
 
             await inter.send(embed=embed, view=view, file=file)
 
+        view.bot_msg = await inter.original_message()
+
+        if await view.wait():
+            await inter.edit_original_message(view=None)
 
     @show.autocomplete('name')
     @build.autocomplete('name')
@@ -431,13 +432,12 @@ class SuperMechs(commands.Cog):
         input = input.lower()
         return [name for name in player.builds if name.lower().startswith(input)]
 
-
     @commands.slash_command()
     @commands.max_concurrency(1, commands.BucketType.user)
     async def buffs(self, inter: disnake.ApplicationCommandInteraction) -> None:
         """Interactive UI for modifying your arena buffs"""
         player = self.get_player(inter)
-        view = ArenaBuffsView(player.arena_buffs)
+        view = ArenaBuffsView(player.arena_buffs, inter.author.id)
 
         async def on_timeout() -> None:
             view.before_stop()
@@ -447,7 +447,6 @@ class SuperMechs(commands.Cog):
 
         await inter.send('**Arena Shop**', view=view)
 
-
     @commands.slash_command(guild_ids=TEST_GUILDS)
     @commands.is_owner()
     async def maxed(self, inter: disnake.ApplicationCommandInteraction) -> None:
@@ -455,7 +454,6 @@ class SuperMechs(commands.Cog):
         me = self.get_player(inter)
         me.arena_buffs.levels.update(ArenaBuffs.maxed().levels)
         await inter.send('Success', ephemeral=True)
-
 
     @commands.slash_command()
     async def compare(self, inter: disnake.ApplicationCommandInteraction, item1: str, item2: str) -> None:
@@ -471,7 +469,6 @@ class SuperMechs(commands.Cog):
 
         if item_a is None or item_b is None:
             raise commands.UserInputError('Either of specified items not found.')
-
 
     @item.autocomplete('name')
     @compare.autocomplete('item1')
@@ -489,7 +486,6 @@ class SuperMechs(commands.Cog):
         return items[:25]
 
 
-
-def setup(bot: HostedBot) -> None:
+def setup(bot: SMBot) -> None:
     bot.add_cog(SuperMechs(bot))
     print('SM loaded')
