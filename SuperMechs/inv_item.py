af8baa98585a4c845d62b94ee39cc5b73055bee2
Enegg <42005170+Enegg@users.noreply.github.com>
2022-05-01 22:32:14 +0200

    implemented power related attributes fixed issue with hacky inheritance

--- SuperMechs/inv_item.py
+++ SuperMechs/inv_item.py
@@ -1,16 +1,41 @@
 from __future__ import annotations
 
+import csv
 import typing as t
 import uuid
 
-from utils import MISSING
+from utils import ProxyType, binary_find_near_index
 
 from .enums import Rarity
+from .errors import MaxPowerReached, MaxTierReached
 from .item import Item
 from .types import Attachment, Attachments, AttachmentType
 
+with (
+    open("SuperMechs/GameData/default_powers.csv", newline="") as file1,
+    open("SuperMechs/GameData/lm_item_powers.csv", newline="") as file2,
+    open("SuperMechs/GameData/reduced_powers.csv", newline="") as file3,
+):
+    rows1 = csv.reader(file1, skipinitialspace=True)
+    rows2 = csv.reader(file2, skipinitialspace=True)
+    rows3 = csv.reader(file3, skipinitialspace=True)
 
-class InvItem(Item[AttachmentType]):
+    DEFAULT_POWERS: dict[Rarity, tuple[int, ...]] = {
+        rarity: tuple(map(int, row)) for rarity, row in zip(Rarity, rows1)
+    }
+
+    LM_POWERS: dict[Rarity, tuple[int, ...]] = {
+        rarity: tuple(map(int, row)) for rarity, row in zip((Rarity.L, Rarity.M), rows2)
+    }
+
+    REDUCED_POWERS: dict[Rarity, tuple[int, ...]] = {
+        rarity: tuple(map(int, row)) for rarity, row in zip((Rarity.L, Rarity.M), rows3)
+    }
+
+REDUCED_COST_ITEMS = {"Archimonde", "Armor Annihilator"}
+
+
+class InvItem(Item[AttachmentType], metaclass=ProxyType, var="underlying"):
     """Represents item inside inventory."""
 
     def __init__(
@@ -18,75 +43,109 @@ class InvItem(Item[AttachmentType]):
         underlying: Item[AttachmentType],
         tier: Rarity,
         power: int = 0,
-        UUID: uuid.UUID = MISSING,
+        UUID: uuid.UUID | None = None,
     ) -> None:
         self.underlying = underlying
         self.tier = tier
         self.power = power
+        self.maxed = tier is Rarity.DIVINE or power >= self.max_power
+        self._level: int | None = None
 
         if isinstance(UUID, uuid.UUID):
             self.UUID = UUID
 
-        elif UUID is MISSING:
+        elif UUID is None:
             self.UUID = uuid.uuid4()
 
         else:
             raise TypeError("Invalid type for UUID passed")
 
-    def __getattr__(self, name: t.Any):
-        try:
-            return getattr(self.underlying, name)
-
-        except AttributeError:
-            raise AttributeError(
-                f'{type(self).__name__} object has no attribute "{name}"'
-            ) from None
-
     def __repr__(self) -> str:
         return (
-            f"<InvItem item={self.underlying!r} tier={self.max_rarity}"
-            f" power={self.power} UUID={self.UUID}>"
+            f"<InvItem item={self.underlying!r} tier={self.tier}"
+            f" power={self.power} UUID={self.UUID} at 0x{id(self):016X}>"
         )
 
     def __hash__(self) -> int:
+        # we don't take into account level, power etc. as there shouldn't ever be
+        # two items with same UUID
+        # technically could exclude the underlying item too
         return hash((self.UUID, self.underlying))
+
+    def add_power(self, power: int) -> None:
+        """Adds power to the item"""
+
+        if self.maxed:
+            raise MaxPowerReached(self)
+
+        if power < 0:
+            raise ValueError("Power cannot be negative")
+
+        if (result := self.power + power) >= self.max_power:
+            result = self.max_power
+            self.maxed = True
+
+        del self.level
+        self.power = result
 
     def can_transform(self) -> bool:
         """Returns True if item has enough power to transform
         and hasn't reached max transform tier, False otherwise"""
-        if 1 + 1 == 2:  # TODO: condition(s) for power
+        if not self.maxed:
             return False
 
-        if self.tier < self.max_rarity:
-            return True
-
-        return False
+        return self.tier < self.rarity.max
 
     def transform(self) -> None:
         """Transforms the item to higher tier, if it has enough power"""
-        if not self.can_transform():
+        if not self.maxed:
             raise ValueError("Not enough power to transform")
+
+        if self.tier is self.rarity.max:
+            raise MaxTierReached(self)
 
         self.tier = self.rarity.next_tier(self.tier)
         self.power = 0
+        self.maxed = self.tier is not Rarity.DIVINE
 
     @property
-    def min_rarity(self) -> Rarity:
-        return self.underlying.rarity.min
+    def level(self) -> int:
+        """The level the item is currently at"""
 
-    @property
-    def max_rarity(self) -> Rarity:
-        return self.underlying.rarity.max
+        if self._level is not None:
+            return self._level
+
+        levels = self.get_bank()[self.tier]
+        self._level = binary_find_near_index(levels, self.power, 0, len(levels))
+        return self._level
+
+    @level.deleter
+    def level(self) -> None:
+        self._level = None
 
     @property
     def max_power(self) -> int:
         """Returns the total power necessary to max the item at current tier"""
-        upper = self.max_rarity
-        lower = self.min_rarity
-        current = self.tier
-        item = self.underlying
 
-        return 0
+        if self.tier is Rarity.DIVINE:
+            return 0
+
+        return self.get_bank()[self.tier][-1]
+
+    def get_bank(self) -> dict[Rarity, tuple[int, ...]]:
+        """Returns the power per level bank for the item"""
+
+        if self.name in REDUCED_COST_ITEMS:
+            return REDUCED_POWERS
+
+        if self.rarity.min >= Rarity.LEGENDARY:
+            return LM_POWERS
+
+        if self.rarity.max <= Rarity.EPIC:
+            # TODO: this has special case too, but currently I have no data on that
+            return DEFAULT_POWERS
+
+        return DEFAULT_POWERS
 
     @classmethod
     def from_item(cls, item: Item[AttachmentType]) -> InvItem[AttachmentType]:
--- utils.py
+++ utils.py
@@ -5,6 +5,7 @@ import re
 import typing as t
 from collections import Counter
 from string import ascii_letters
+from typing_extensions import Self
 
 SupportsSet = t.TypeVar("SupportsSet", bound=t.Hashable)
 T = t.TypeVar("T")
@@ -31,6 +32,42 @@ class _MissingSentinel:
 
 
 MISSING: t.Final[t.Any] = _MissingSentinel()
+
+
+class ProxyType(type):
+    """Metaclass for creating false inheritance objects.
+    The point of such action is to avoid repeating code and simply using `__getattr__`
+    to do the hard work, while retaining all the static typing benefits."""
+
+    def __new__(
+        cls,
+        name: str,
+        bases: tuple[type, ...],
+        dict: dict[str, t.Any],
+        /,
+        **kwargs: t.Any,
+    ) -> Self:
+        if "__orig_bases__" in dict:
+            bases = (t.Generic,)
+
+        else:
+            bases = ()
+
+        if "var" in kwargs:
+            attribute = kwargs.pop("var")
+
+            def __getattr__(self: Self, name: t.Any) -> t.Any:
+                try:
+                    return getattr(getattr(self, attribute), name)
+
+                except AttributeError:
+                    raise AttributeError(
+                        f'{type(self).__name__} object has no attribute "{name}"'
+                    ) from None
+
+            dict["__getattr__"] = __getattr__
+
+        return super().__new__(cls, name, bases, dict, **kwargs)
 
 
 async def no_op(*args: t.Any, **kwargs: t.Any) -> None:
@@ -94,3 +131,19 @@ def format_count(it: t.Iterable[t.Any], /) -> t.Iterator[str]:
 def random_str(length: int) -> str:
     """Generates a random string of given length from ascii letters"""
     return "".join(random.sample(ascii_letters, length))
+
+
+def binary_find_near_index(container: t.Sequence[int], value: int, start: int, end: int) -> int:
+    """Binary search for the index of the largest value in container, container[index] <= value"""
+    index = (start + end) // 2
+
+    if end - start <= 1:
+        return index
+
+    if container[index] > value:
+        return binary_find_near_index(container, value, start, index)
+
+    if container[index] < value:
+        return binary_find_near_index(container, value, index, end)
+
+    return index
