fa5fdd582d3ec2a2653c922c22c7349aa037e39a
Enegg <42005170+Enegg@users.noreply.github.com>
2022-11-11 22:13:38 +0100

    Add `__all__`

--- shared/__init__.py
+++ shared/__init__.py
@@ -7,5 +7,8 @@ if t.TYPE_CHECKING:
     from aiohttp import ClientSession
 
 
+__all__ = ("SESSION_CTX",)
+
+
 SESSION_CTX: ContextVar["ClientSession"] = ContextVar("session")
 """The aiohttp.ClientSession instantiated by the bot."""
