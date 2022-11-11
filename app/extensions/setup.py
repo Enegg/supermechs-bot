db619013907cce16455bb287e6bd05b24d7696f5
Enegg <42005170+Enegg@users.noreply.github.com>
2022-11-11 22:17:01 +0100

    Extract ext functionality into subcommands

--- app/extensions/setup.py
+++ app/extensions/setup.py
@@ -1,15 +1,19 @@
 from __future__ import annotations
 
+import asyncio
 import io
+import traceback
 import typing as t
+from contextlib import redirect_stderr, redirect_stdout
 from contextvars import ContextVar
 from traceback import print_exception
 
-from disnake import AllowedMentions, CommandInteraction
+from disnake import AllowedMentions, CommandInteraction, File, ModalInteraction, TextInputStyle
 from disnake.ext import commands, plugins
+from disnake.ui import TextInput
 
 from app.config import TEST_GUILDS
-from app.library_extensions import str_to_file
+from app.library_extensions import ensure_file
 
 if t.TYPE_CHECKING:
     from app.bot import SMBot
@@ -21,31 +25,20 @@ LAST_EXTENSION = ContextVar[str | None]("last_extension", default=None)
 @plugin.slash_command()
 @commands.default_member_permissions(administrator=True)
 @commands.is_owner()
-async def ext(
-    inter: CommandInteraction,
-    ext: str | None = None,
-    action: t.Literal["load", "reload", "unload"] = "reload",
-) -> None:
-    """Extension manager.
+async def ext(inter: CommandInteraction) -> None:
+    pass
 
-    Parameters
-    -----------
-    action: The type of action to perform.
-    ext: The name of extension to perform action on.
-    """
+
+async def _ext_helper(
+    inter: CommandInteraction, ext: str | None, func: t.Callable[[str], None]
+) -> None:
     ext = ext or LAST_EXTENSION.get()
 
     if ext is None:
         return await inter.response.send_message("No extension cached.", ephemeral=True)
 
-    funcs = dict(
-        load=plugin.bot.load_extension,
-        reload=plugin.bot.reload_extension,
-        unload=plugin.bot.unload_extension,
-    )
-
     try:
-        funcs[action](ext)
+        func(ext)
 
     except commands.ExtensionError as error:
         with io.StringIO() as sio:
@@ -55,11 +48,46 @@ async def ext(
             await inter.send(sio.getvalue(), ephemeral=True)
 
     else:
-        await inter.response.send_message("Success", ephemeral=True)
         LAST_EXTENSION.set(ext)
+        await inter.response.send_message("Success", ephemeral=True)
 
 
-@ext.autocomplete("ext")
+@ext.sub_command()
+async def load(inter: CommandInteraction, ext: str | None = None) -> None:
+    """Load an extension.
+
+    Parameters
+    ----------
+    ext: The name of extension to perform action on.
+    """
+    await _ext_helper(inter, ext, plugin.bot.load_extension)
+
+
+@ext.sub_command()
+async def reload(inter: CommandInteraction, ext: str | None = None) -> None:
+    """Reload an extension.
+
+    Parameters
+    ----------
+    ext: The name of extension to perform action on.
+    """
+    await _ext_helper(inter, ext, plugin.bot.reload_extension)
+
+
+@ext.sub_command()
+async def unload(inter: CommandInteraction, ext: str | None = None) -> None:
+    """Unload an extension.
+
+    Parameters
+    ----------
+    ext: The name of extension to perform action on.
+    """
+    await _ext_helper(inter, ext, plugin.bot.unload_extension)
+
+
+@load.autocomplete("ext")
+@reload.autocomplete("ext")
+@unload.autocomplete("ext")
 async def ext_autocomplete(_: CommandInteraction, input: str) -> list[str]:
     input = input.lower()
     return [ext for ext in plugin.bot.extensions if input in ext.lower()]
@@ -113,39 +141,63 @@ async def raise_autocomplete(_: CommandInteraction, input: str) -> list[str]:
 @plugin.slash_command(name="eval")
 @commands.default_member_permissions(administrator=True)
 @commands.is_owner()
-async def eval_(inter: CommandInteraction, input: str) -> None:
+async def eval_(inter: CommandInteraction | ModalInteraction, input: str | None = None) -> None:
     """Evaluates the given input as code.
 
     Parameters
     ----------
     input: code to execute.
     """
-    input = input.strip()
+    if input is None:
+        text_input = TextInput(
+            label="Code to evaluate", custom_id="eval:input", style=TextInputStyle.multi_line
+        )
+        await inter.response.send_modal(
+            title="Prompt", custom_id="eval:modal", components=text_input
+        )
 
-    out = io.StringIO()
+        def check(inter: ModalInteraction) -> bool:
+            return inter.custom_id == "eval:modal"
 
-    def print_(*args: t.Any, **kwargs: t.Any) -> None:
-        print(*args, **kwargs, file=out)
+        try:
+            modal_inter: ModalInteraction = await plugin.bot.wait_for(
+                "modal_submit", check=check, timeout=600
+            )
 
-    output = eval(input, globals() | {"bot": plugin.bot, "inter": inter, "print": print_})
-    std = out.getvalue()
+        except asyncio.TimeoutError:
+            return await inter.send("Modal timed out.", ephemeral=True)
 
-    if output is None or not std:
-        msg = f"```\n{std or output}```"
+        input = modal_inter.text_values[text_input.custom_id]
+        inter = modal_inter
 
-    else:
-        msg = f"```\n{output}```\n**stdout:**\n```\n{std}```"
+    input = input.removeprefix("```py").removesuffix("```").strip()
 
-    if len(msg) > 2000:
-        file = str_to_file(msg.strip("` "))
-        await inter.send(file=file, ephemeral=True)
+    with io.StringIO() as local_stdout:
+        with redirect_stdout(local_stdout), redirect_stderr(local_stdout):
+            try:
+                exec(input, globals() | {"bot": plugin.bot, "inter": inter})
 
-    else:
+            except Exception as ex:
+                traceback.print_exception(type(ex), ex, ex.__traceback__, file=local_stdout)
+
+        if local_stdout.tell() == 0:
+            text = "No output."
+
+        else:
+            text = local_stdout.getvalue()
+
+    line = "```\n{}```"
+
+    if len(text) + len(line) - 2 <= 2000:
         await inter.send(
-            msg,
+            line.format(text),
             allowed_mentions=AllowedMentions.none(),
             ephemeral=True,
         )
+
+    else:
+        file = File(ensure_file(text), "output.txt")
+        await inter.send(file=file, ephemeral=True)
 
 
 setup, teardown = plugin.create_extension_handlers()
