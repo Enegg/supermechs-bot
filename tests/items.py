8ddd897740def8cde5d522fdddecae21dbc4ed79
Enegg <42005170+Enegg@users.noreply.github.com>
2023-04-06 01:52:55 +0200

    Move data files to hidden folder Update scarse tests

--- tests/items.py
+++ tests/items.py
@@ -1,18 +1,18 @@
-import json
+import orjson
 
 from app.abstract.files import File
-from app.SuperMechs.images import AttachedImage, Attachment
+# from app.SuperMechs.images import AttachedImage, Attachment
 from app.SuperMechs.inv_item import InvItem
 from app.SuperMechs.item import Item
 from app.SuperMechs.typedefs import ItemDictVer3
 
 
 with open("tests/example_item_v2.json") as file:
-    data: ItemDictVer3 = json.load(file)
+    data: ItemDictVer3 = orjson.loads(file.read())
 
-resource = File("D:/Obrazy/Games/SuperMechs/Sprites/Heat items/OverchargedRocketBattery.png")
+resource = File("./data/OverchargedRocketBattery.png")
 
-image, coro = AttachedImage.from_resource(resource, Attachment(0, 0))
+# image, coro = AttachedImage.from_resource(resource, Attachment(0, 0))
 
-item = Item[Attachment].from_json(data, "@darkstare", False, image)
-inv_item = InvItem.from_item(item)
+item = Item.from_json(data, "@darkstare", False)
+inv_item = InvItem.from_item(item, maxed=True)
--- tests/pack_interface.py
+++ tests/pack_interface.py
@@ -3,25 +3,19 @@ import typing as t
 
 from app.abstract.files import URL
 from app.shared import SESSION_CTX
-from app.SuperMechs.pack_interface import PackInterface
-from app.SuperMechs.urls import PACK_V2_URL
+from app.SuperMechs.pack_interface import ItemPack
+from app.SuperMechs.urls import PACK_V2
 
 logging.basicConfig(level=logging.DEBUG)
 
-# pack_v1_manager = PackInterface(DEFAULT_PACK_URL)
-# pack_v2_manager = PackInterface(DEFAULT_PACK_V2_URL)
-# local_manager = PackInterface("C:/Real_Shit_Estate/Item-packs/items.json")
 
-
-async def runner(link: str = PACK_V2_URL, **extra: t.Any):
+async def runner(link: str = PACK_V2, **extra: t.Any):
     from aiohttp import ClientSession
 
     # import yarl
 
     logging.basicConfig(level="INFO")
-    interface = PackInterface()
     async with ClientSession() as session:
         SESSION_CTX.set(session)
-        resource = URL(link)
-        await interface.load(resource, **extra)
+        interface = ItemPack.from_json(await URL(link).json())
         return interface
