f43b5a3dbaff1815890a44542db8dc9dc1103648
Enegg <42005170+Enegg@users.noreply.github.com>
2023-04-06 11:14:41 +0200

    send traceback when in debug

--- app/library_extensions/error_handling.py
+++ app/library_extensions/error_handling.py
@@ -6,31 +6,31 @@ from functools import partial
 from disnake import Client, CommandInteraction, File, LocalizationProtocol
 from disnake.abc import Messageable
 from disnake.ext import commands
+from disnake.ext.commands.common_bot_base import CommonBotBase
 from disnake.utils import MISSING
 
-from .text_formatting import localized_text
+from .text_formatting import Markdown, localized_text
 
 __all__ = ("setup_channel_logger",)
 
 
-def exception_to_message(text: str, exception: Exception) -> tuple[str, File]:
-    lines = traceback.format_exception(exception)
-    total_length = sum(map(len, lines))
-    traceback_text = "".join(lines)
+def exception_to_message(content: str, exception: BaseException) -> tuple[str, File]:
+    """Formats exception data and returns message content & file to send a message with.
 
-    if len(text) + total_length + 10 > 2000:
+    Formatted traceback is appended to text if the resulting string stays under 2000 character
+    limit, otherwise creates a File and returns text unaffected.
+    """
+    traceback_text = "".join(traceback.format_exception(exception))
+
+    if len(content) + len(traceback_text) + 10 > 2000:
         bio = io.BytesIO(traceback_text.encode())
         file = File(bio, "traceback.py")
 
     else:
         file = MISSING
-        text = (
-            f"{text}\n"
-            "```py\n"
-            f"{traceback_text}```"
-        )
+        content = f"{content}\n{Markdown.codeblock(traceback_text), 'py'}"
 
-    return text, file
+    return content, file
 
 
 async def on_slash_command_error(
@@ -41,6 +41,8 @@ async def on_slash_command_error(
     inter: CommandInteraction,
     error: commands.CommandError,
 ) -> None:
+    file = MISSING
+
     if isinstance(error, commands.NotOwner):
         info = localized_text("This is a developer-only command.", "CMD_DEV", i18n, inter.locale)
 
@@ -67,28 +69,28 @@ async def on_slash_command_error(
         text = (
             f"{error}\n"
             f"Place: `{inter.guild or inter.channel}`\n"
-            f"Command invocation: {inter.author.mention} ({inter.author.display_name})"
-            f" `/{inter.application_command.qualified_name}` {arguments}"
+            f"Command invocation: {inter.author.mention} ({inter.author.display_name}) "
+            f"`/{inter.application_command.qualified_name}` {arguments}"
         )
+        message_content, file = exception_to_message(text, error)
 
         if __debug__:
-            info = text
+            info = message_content
 
         else:
-            message_text, file = exception_to_message(text, error)
-            await channel.send(message_text, file=file)
+            await channel.send(message_content, file=file)
             logger.exception(text, exc_info=error)
 
             info = localized_text(
                 "Command executed with an error...", "CMD_ERROR", i18n, inter.locale
             )
 
-    await inter.send(info, ephemeral=True)
+    await inter.send(info, file=file, ephemeral=True)
 
 
 async def setup_channel_logger(client: Client, channel_id: int, logger: logging.Logger) -> None:
-    """"""
-    assert isinstance(client, commands.BotBase)
+    """Creates an on_slash_command_error listener which sends tracebacks to selected channel."""
+    assert isinstance(client, CommonBotBase)
     channel = await client.fetch_channel(channel_id)
 
     if not isinstance(channel, Messageable):
