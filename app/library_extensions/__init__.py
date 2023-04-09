03e7ae2d54e2ad5a1b97ca6e5b5eef3dcaa2a4b7
Enegg <42005170+Enegg@users.noreply.github.com>
2023-04-09 18:46:10 +0200

    yeet interaction context for now

--- app/library_extensions/__init__.py
+++ app/library_extensions/__init__.py
@@ -3,6 +3,6 @@ Utility module providing functions and classes related to disnake
 and/or interacting with discord.
 """
 from .error_handling import *
-from .interaction_context import *
+# from .interaction_context import *
 from .pending import *
 from .text_formatting import *
