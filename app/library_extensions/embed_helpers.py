ce7c7ce15ea84e260a17f8b47f830e7b8e64a021
Enegg <42005170+Enegg@users.noreply.github.com>
2023-04-14 15:12:42 +0200

    Create embed helpers module

---
+++ app/library_extensions/embed_helpers.py
@@ -0,0 +1,30 @@
+import typing as t
+
+from disnake import Embed, File
+
+from abstract.files import Bytes
+
+if t.TYPE_CHECKING:
+    from PIL.Image import Image
+
+__all__ = ("embed_image", "embed_to_footer")
+
+
+def embed_to_footer(embed: Embed) -> None:
+    """Utility/debug function useful for introspecting embed's content."""
+
+    parts: list[str] = ["Debug:", f"Size: {len(embed)}"]
+
+    if url := embed.image.url:
+        parts.append(f"Image: {url}")
+
+    if url := embed.thumbnail.url:
+        parts.append(f"Thumb: {url}")
+
+    embed.set_footer(text="\n".join(parts))
+
+
+def embed_image(image: "Image", filename: str) -> tuple[str, File]:
+    """Creates a File and returns it with an attachment url."""
+    resource = Bytes.from_image(image, filename)
+    return str(resource.url), File(resource.fp, filename)
