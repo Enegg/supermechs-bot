a9fa603b2b2d690937b07a57651b3f01ab1954f4
Enegg <42005170+Enegg@users.noreply.github.com>
2023-06-22 17:03:24 +0200

    Dynaconf

--- app/config.py
+++ app/config.py
@@ -1,5 +1,13 @@
 from configparser import ConfigParser
 
+from dynaconf import Dynaconf
+
+settings = Dynaconf(
+    envvar_prefix="SM",
+    settings_files=["settings.toml", ".secrets.toml", "app/assets/emojis.toml", "app/assets/urls.toml"],
+    load_dotenv=True,
+)
+
 parser = ConfigParser()
 parser.read("config.ini")
 
@@ -12,3 +20,7 @@ HOME_GUILD_ID = parser.getint("bot", "HOME_GUILD_ID")
 
 TEST_GUILDS = (HOME_GUILD_ID,)
 """The IDs of guilds the bot will register commands in while in dev mode."""
+
+
+# `envvar_prefix` = export envvars with `export DYNACONF_FOO=bar`.
+# `settings_files` = Load these files in the order.
