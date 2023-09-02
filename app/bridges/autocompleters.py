3f1d2f0ce36172c0ae1eae89084624c0898bcdf4
Enegg <42005170+Enegg@users.noreply.github.com>
2023-09-03 01:14:21 +0200

    Yeet abbrevs for the while

--- app/bridges/autocompleters.py
+++ app/bridges/autocompleters.py
@@ -2,7 +2,6 @@ import typing as t
 
 from library_extensions import OPTION_LIMIT
 from managers import item_pack_manager, player_manager
-from user_input import sanitize_string
 
 from supermechs.api import Element, ItemData, Type
 from supermechs.typedefs import Name
@@ -36,7 +35,8 @@ async def item_name_autocomplete(inter: "CommandInteraction", input: str) -> Aut
     pack = item_pack_manager.mapping["@Darkstare"]  # TODO: replace with default pack reference
 
     filters = _get_item_filters(inter.filled_options)
-    abbrevs = pack.name_abbrevs.get(input.lower(), set())
+    # abbrevs = set()
+    # TODO: reintroduce abbreviations
 
     def filter_item_names(names: t.Iterable[Name]) -> t.Iterator[Name]:
         items = map(pack.get_item_by_name, names)
@@ -47,21 +47,22 @@ async def item_name_autocomplete(inter: "CommandInteraction", input: str) -> Aut
         return (item.name for item in items)
 
     # place matching abbreviations at the top
-    matching_item_names = sorted(filter_item_names(abbrevs))
+    # matching_item_names = sorted(filter_item_names(abbrevs))
 
     # this shouldn't ever happen, but handle it anyway
-    if len(matching_item_names) >= OPTION_LIMIT:
-        del matching_item_names[OPTION_LIMIT:]
-        return matching_item_names
+    # if len(matching_item_names) >= OPTION_LIMIT:
+    #     del matching_item_names[OPTION_LIMIT:]
+    #     return matching_item_names
 
     # extra filter to exclude duplicates
-    filters.append(lambda item: item.name not in abbrevs)
+    # filters.append(lambda item: item.name not in abbrevs)
 
     import heapq
 
     # extend names up to OPTION_LIMIT
-    matching_item_names += heapq.nsmallest(
-        OPTION_LIMIT - len(matching_item_names),
+    matching_item_names = heapq.nsmallest(
+        # OPTION_LIMIT - len(matching_item_names),
+        OPTION_LIMIT,
         filter_item_names(search_for(input, pack.iter_item_names())),
     )
     return matching_item_names
@@ -71,16 +72,6 @@ async def mech_name_autocomplete(inter: "CommandInteraction", input: str) -> Aut
     """Autocomplete for player builds."""
 
     player = player_manager.lookup_or_create(inter.author)
-    case_insensitive = input.lower()
+    lowercase = input.lower()
 
-    matching = [name for name in player.builds if name.lower().startswith(case_insensitive)]
-
-    if matching:
-        return matching
-
-    input = sanitize_string(input)
-
-    if input:
-        return {f'Enter to create mech "{input}"...': input}
-
-    return []
+    return [name for name in player.builds if name.lower().startswith(lowercase)]
