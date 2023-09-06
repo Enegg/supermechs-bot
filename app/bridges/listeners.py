dccf28162a000e23cf2acd862705a72ef4e1c3b9
Enegg <42005170+Enegg@users.noreply.github.com>
2023-09-06 22:55:56 +0200

    Fix event not receiving arguments

--- app/bridges/listeners.py
+++ app/bridges/listeners.py
@@ -1,7 +1,8 @@
 import logging
 import typing as t
+from functools import partial
 
-from disnake import Client, CommandInteraction
+from disnake import Client, CommandInteraction, Event
 from disnake.ext.commands.common_bot_base import CommonBotBase
 
 from config import DATE_FORMAT
@@ -29,5 +30,6 @@ async def on_application_command(interaction: CommandInteraction, /) -> None:
 
 def register_listeners(bot: CommonBotBase[t.Any], /) -> None:
     """Entry point for registering listeners."""
-    bot.add_listener(on_ready)
+    assert isinstance(bot, Client)
+    bot.add_listener(partial(on_ready, bot), Event.ready)
     bot.add_listener(on_application_command)
