630cb493f145bdd65bac1f1e8332d73e97f2c4c3
Enegg <42005170+Enegg@users.noreply.github.com>
2023-09-09 00:40:52 +0200

    Compute acronyms

--- app/bridges/autocompleters.py
+++ app/bridges/autocompleters.py
@@ -5,14 +5,26 @@ from managers import item_pack_manager, player_manager
 
 from supermechs.api import Element, ItemData, Type
 from supermechs.typedefs import Name
-from supermechs.utils import search_for
+from supermechs.utils import acronym_of, search_for
 
 if t.TYPE_CHECKING:
     from disnake import CommandInteraction, Localized
 
 __all__ = ("item_name_autocomplete", "mech_name_autocomplete")
 
-AutocompleteRetT = t.Sequence["str | Localized[str]"] | t.Mapping[str, "str | Localized[str]"]
+AutocompleteReturnType = t.Sequence["str | Localized[str]"] | t.Mapping[str, "str | Localized[str]"]
+
+acronyms: t.Mapping[str, set[Name]] = {}
+
+
+def _make_acronyms(names: t.Iterable[Name], /) -> None:
+    for name in names:
+        if acronym := acronym_of(name):
+            try:
+                acronyms[acronym].add(name)
+
+            except KeyError:
+                acronyms[acronym] = {name}
 
 
 def _get_item_filters(options: t.Mapping[str, t.Any], /) -> list[t.Callable[[ItemData], bool]]:
@@ -29,14 +41,15 @@ def _get_item_filters(options: t.Mapping[str, t.Any], /) -> list[t.Callable[[Ite
     return filters
 
 
-async def item_name_autocomplete(inter: "CommandInteraction", input: str) -> AutocompleteRetT:
+async def item_name_autocomplete(inter: "CommandInteraction", input: str) -> AutocompleteReturnType:
     """Autocomplete for items with regard for type & element."""
 
     pack = item_pack_manager["@Darkstare"]  # TODO: replace with default pack reference
 
     filters = _get_item_filters(inter.filled_options)
-    # abbrevs = set()
-    # TODO: reintroduce abbreviations
+
+    if not acronyms:
+        _make_acronyms(pack.item_names)
 
     def filter_item_names(names: t.Iterable[Name]) -> t.Iterator[Name]:
         items = map(pack.get_item_by_name, names)
@@ -47,28 +60,31 @@ async def item_name_autocomplete(inter: "CommandInteraction", input: str) -> Aut
         return (item.name for item in items)
 
     # place matching abbreviations at the top
-    # matching_item_names = sorted(filter_item_names(abbrevs))
+    if items := acronyms.get(input.lower()):
+        matching_item_names = sorted(filter_item_names(items))
 
-    # this shouldn't ever happen, but handle it anyway
-    # if len(matching_item_names) >= OPTION_LIMIT:
-    #     del matching_item_names[OPTION_LIMIT:]
-    #     return matching_item_names
+        # this shouldn't ever happen, but handle it anyway
+        if len(matching_item_names) >= OPTION_LIMIT:
+            del matching_item_names[OPTION_LIMIT:]
+            return matching_item_names
 
-    # extra filter to exclude duplicates
-    # filters.append(lambda item: item.name not in abbrevs)
+        # extra filter to exclude duplicates
+        filters.append(lambda item: item.name not in items)
+
+    else:
+        matching_item_names = []
 
     import heapq
 
     # extend names up to OPTION_LIMIT
-    matching_item_names = heapq.nsmallest(
-        # OPTION_LIMIT - len(matching_item_names),
-        OPTION_LIMIT,
-        filter_item_names(search_for(input, pack.iter_item_names())),
+    matching_item_names += heapq.nsmallest(
+        OPTION_LIMIT - len(matching_item_names),
+        filter_item_names(search_for(input, pack.item_names)),
     )
     return matching_item_names
 
 
-async def mech_name_autocomplete(inter: "CommandInteraction", input: str) -> AutocompleteRetT:
+async def mech_name_autocomplete(inter: "CommandInteraction", input: str) -> AutocompleteReturnType:
     """Autocomplete for player builds."""
 
     player = player_manager(inter.author)
