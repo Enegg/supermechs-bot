8fa4dd636fc084ea83989aafb15d7e721decf1b0
Enegg <42005170+Enegg@users.noreply.github.com>
2024-01-25 00:26:39 +0100

    typing => collections.abc

--- app/typeshed.py
+++ app/typeshed.py
@@ -1,24 +1,25 @@
 import os
 import typing as t
 import typing_extensions as tex
+from collections import abc
 
 T = tex.TypeVar("T", infer_variance=True)
 T2 = tex.TypeVar("T2", infer_variance=True)
 RetT = tex.TypeVar("RetT", infer_variance=True)
-KT = t.TypeVar("KT", bound=t.Hashable)
+KT = tex.TypeVar("KT", bound=abc.Hashable)
 """Key-type of a mapping."""
-VT = t.TypeVar("VT")
+VT = tex.TypeVar("VT")
 """Value-type of a mapping."""
-P = t.ParamSpec("P")
+P = tex.ParamSpec("P")
 """Parameter specification of a callable."""
 
 twotuple = tuple[T, T]
 """Tuple of two elements of same type."""
 XOrTupleXY = T | tuple[T, T2]
 """Type or tuple of two types."""
-Factory = t.Callable[[], T]
+Factory = abc.Callable[[], T]
 """0-argument callable returning an object of given type."""
 LiteralURL: t.TypeAlias = str
 """String representing a URL."""
 Pathish: t.TypeAlias = os.PathLike[str] | str
-CoroFunc: t.TypeAlias = t.Callable[..., t.Coroutine[t.Any, t.Any, T]]
+CoroFunc: t.TypeAlias = abc.Callable[..., abc.Coroutine[t.Any, t.Any, T]]
