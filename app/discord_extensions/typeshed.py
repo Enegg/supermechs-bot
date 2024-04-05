243670967c3b7007d02b611511016dcfba6a23cb
Enegg <42005170+Enegg@users.noreply.github.com>
2024-04-05 03:08:48 +0200

    Include `SenderKeywords`

--- app/discord_extensions/typeshed.py
+++ app/discord_extensions/typeshed.py
@@ -1,20 +1,28 @@
 import typing
 from collections import abc
 
-from disnake import Event, Localized
+import disnake
 
 from typeshed import CoroFunc
 
-__all__ = ("AutocompleteReturnType", "ListenerRegistry")
+__all__ = ("AutocompleteReturnType", "ListenerRegistry", "SenderKeywords")
 
 AutocompleteReturnType: typing.TypeAlias = (
-    abc.Sequence[str | Localized[str]] | abc.Mapping[str, str | Localized[str]]
+    abc.Sequence[str | disnake.Localized[str]] | abc.Mapping[str, str | disnake.Localized[str]]
 )
 
 
+@typing.runtime_checkable
 class ListenerRegistry(typing.Protocol):
-    def add_listener(self, func: CoroFunc[typing.Any], /, name: str | Event = ...) -> None:
+    def add_listener(self, func: CoroFunc[...], /, name: str | disnake.Event = ...) -> None:
         ...
 
-    def remove_listener(self, func: CoroFunc[typing.Any], /, name: str | Event = ...) -> None:
+    def remove_listener(self, func: CoroFunc[...], /, name: str | disnake.Event = ...) -> None:
         ...
+
+
+class SenderKeywords(typing.TypedDict, total=False):
+    content: str
+    embed: disnake.Embed
+    file: disnake.File
+    suppress_embeds: bool
