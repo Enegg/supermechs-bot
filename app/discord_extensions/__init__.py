11ba1c1e04741e92bc596ed8d9be68edf1218e4c
Enegg <42005170+Enegg@users.noreply.github.com>
2024-04-16 01:07:09 +0200

    Update `__init__`

--- app/discord_extensions/__init__.py
+++ app/discord_extensions/__init__.py
@@ -1,13 +1,11 @@
 """
-Utility module providing functions and classes related to the API wrapper
-and/or interacting with discord.
+Utility functions and classes related to the API wrapper and/or interacting with discord.
 """
 from .commands import *
-from .embed_utils import *
-from .error_handling import *
+from .embeds import *
 from .extensions import *
-from .file_utils import *
+from .files import *
 from .limits import *
 from .pending import *
-from .text_utils import *
+from .text import *
 from .typeshed import *
