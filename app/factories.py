c53d56d9803f6adcd0ad02e5b2e21135d37b32ab
Enegg <42005170+Enegg@users.noreply.github.com>
2024-05-24 18:59:13 +0200

    Guard from large images

--- app/factories.py
+++ app/factories.py
@@ -11,6 +11,7 @@ from supermechs.ext.deserializers import to_item_pack
 from supermechs.ext.deserializers.typedefs import AnyItemPack
 
 if typing.TYPE_CHECKING:
+    from aiohttp.typedefs import StrOrURL
     from PIL import Image
 
 _LOGGER = logging.getLogger(__name__)
@@ -29,17 +30,36 @@ def item_pack_factory(data: AnyItemPack, /) -> ItemPack:
     return pack
 
 
+MAX_CONTENT_LENGTH = 1024**2 * 25  # 25MiB
+
+
 @async_memoize
-async def image_factory(url: str, /) -> "Image.Image":
+async def load_image(url: "StrOrURL", /) -> "Image.Image":
     _LOGGER.debug("Requesting %s", url)
 
     from PIL import ImageFile
 
     async with IO_SESSION.get().get(url) as response:
         response.raise_for_status()
+        _LOGGER.debug(
+            "Content type: %s, length: %s", response.content_type, response.content_length
+        )
+        if not response.content_type.startswith("image"):
+            msg = "Content type is not image"
+            raise ValueError(msg)
+
+        if response.content_length is not None and response.content_length > MAX_CONTENT_LENGTH:
+            msg = "Image too large"
+            raise ValueError(msg)
+
         parser = ImageFile.Parser()
 
         async for chunk, _ in response.content.iter_chunks():
             parser.feed(chunk)
+            assert parser.data is not None
+
+            if len(parser.data) > MAX_CONTENT_LENGTH:
+                msg = "Image too large"
+                raise ValueError(msg)
 
         return parser.close()
