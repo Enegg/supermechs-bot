e8fd77bfe0215b49111b236dd5d8e036fa00a8c0
Eneg <42005170+Enegg@users.noreply.github.com>
2024-05-30 22:17:34 +0200

    Modify models

--- app/models/player.py
+++ app/models/player.py
@@ -1,8 +1,10 @@
+import typing
 import uuid
 from collections import abc
+from datetime import datetime
 
-import disnake
 from attrs import define, field
+from disnake.utils import get as get_matching, utcnow
 
 from .mech_build import MechBuild
 
@@ -16,46 +18,39 @@ __all__ = ("Player",)
 class Player:
     """Represents a SuperMechs player."""
 
-    user: disnake.abc.User = field()
-    builds: abc.MutableMapping[uuid.UUID, MechBuild] = field(factory=dict)
-    arena_shop: ArenaShop = field(factory=arena_shop, init=False)
-    _active_build: MechBuild | None = field(default=None, init=False)
+    id: typing.Final[int] = field()
+    builds: typing.Final[abc.MutableMapping[uuid.UUID, MechBuild]] = field(factory=dict)
+    arena_shop: typing.Final[ArenaShop] = field(factory=arena_shop)
+    created_at: typing.Final[datetime] = field(factory=utcnow)
+    _recent_uuid: uuid.UUID | None = field(default=None, init=False)
 
     @property
-    def name(self) -> str:
-        return self.user.name
+    def recent_build(self) -> MechBuild | None:
+        return None if self._recent_uuid is None else self.builds[self._recent_uuid]
 
-    @property
-    def active_build(self) -> MechBuild | None:
-        return self._active_build
-
-    @active_build.setter
-    def active_build(self, mech: MechBuild) -> None:
+    @recent_build.setter
+    def recent_build(self, mech: MechBuild) -> None:
         if mech.id not in self.builds:
-            msg = "Active build set to a mech not belonging to the player"
+            msg = "Recent build set to a mech not belonging to the player"
             raise ValueError(msg)
 
-        self._active_build = mech
+        self._recent_uuid = mech.id
 
     def get_build_by_name(self, name: str, /) -> MechBuild | None:
         """Retrieve a build with given name."""
-        for build in self.builds.values():
-            if build.name == name:
-                return build
+        return get_matching(self.builds.values(), name=name)
 
-        return None
-
-    def get_active_or_create_build(self, possible_name: str | None = None, /) -> MechBuild:
-        """Retrieves active build if the player has one, otherwise creates a new one.
+    def get_recent_or_create_build(self, possible_name: str | None = None, /) -> MechBuild:
+        """Return recent build if the player has one, or create a new one.
 
         Parameters
         ----------
-        possible_name: The name to create a new build with. Ignored if there's an active build.
+        possible_name: The name to create a new build with. Ignored if there's a recent build.
         """
-        if self.active_build is None:
-            return self.create_build(possible_name)
+        if (recent := self.recent_build) is not None:
+            return recent
 
-        return self.active_build
+        return self.create_build(possible_name)
 
     def get_or_create_build(self, name: str, /) -> MechBuild:
         """Retrieves existing build under given name, otherwise creates a new one.
@@ -71,15 +66,20 @@ class Player:
 
         return self.create_build(name)
 
-    def create_build(self, name: str | None = None, /, mech: Mech | None = None) -> MechBuild:
-        """Creates a new build, sets it as active and returns it.
+    def load_build(self, name: str, mech: Mech) -> None:
+        build = MechBuild(mech, name)
+        self.builds[build.id] = build
+
+    def create_build(self, name: str | None = None, /) -> MechBuild:
+        """Creates a new build, sets it as recent and returns it.
 
         Parameters
         ----------
         name: The name to assign to the build. Defaults to `"Unnamed Mech"`.
         """
         build = MechBuild() if name is None else MechBuild(name=name)
-        self.builds[build.id] = self._active_build = build
+        self.builds[build.id] = build
+        self._recent_uuid = build.id
         return build
 
     def rename_build(self, uuid: uuid.UUID, name: str) -> None:
