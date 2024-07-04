0bbd2a4f691a7db387720099544ab6c8cd4acffc
Enegg <42005170+Enegg@users.noreply.github.com>
2024-07-04 16:37:48 +0200

    new models

---
+++ app/models/graphics.py
@@ -0,0 +1,117 @@
+import logging
+import typing
+
+import anyio.lowlevel
+import attrs
+from PIL import Image
+
+from async_utils import async_memoize
+from config import CONFIG
+from resources import HttpResource, Resource
+
+from supermechs.graphics.joints import Joints
+
+__all__ = ("AbstractSprite", "SheetSprite", "SingleSprite")
+
+_LOGGER = logging.getLogger("factories")
+
+
+@async_memoize
+async def load_image(resource: Resource, /) -> "Image.Image":
+    from PIL import ImageFile
+
+    _LOGGER.debug("GET %s", resource.uri)
+
+    parser = ImageFile.Parser()
+
+    async for chunk in resource.iter_chunked(CONFIG.chunk_size):
+        parser.feed(chunk)
+        assert parser.data is not None
+
+        if len(parser.data) > CONFIG.max_image_size:  # pyright: ignore[reportUnknownArgumentType]
+            msg = "Image too large"
+            raise ValueError(msg)
+
+    return parser.close()
+
+
+def assure_rgba(image: Image.Image, /) -> Image.Image:
+    if image.mode == "RGBA":
+        return image
+
+    return image.convert("RGBA")
+
+
+def resize(image: Image.Image, width: int = 0, height: int = 0) -> Image.Image:
+    width = width or image.width
+    height = height or image.height
+
+    if image.size == (width, height):
+        return image
+
+    return image.resize((width, height))
+
+
+class AbstractSprite(typing.Protocol):
+    @property
+    def url(self) -> str | None: ...
+
+    @property
+    def joints(self) -> Joints: ...
+
+    async def load(self) -> Image.Image: ...
+
+
+@attrs.define(kw_only=True)
+class ImageLoader:
+    resource: Resource
+    target_size: tuple[int, int] = (0, 0)
+    _image: Image.Image | None = attrs.field(default=None, init=False)
+
+    async def load(self) -> Image.Image:
+        if self._image is not None:
+            await anyio.lowlevel.checkpoint()
+            return self._image
+
+        image = await load_image(self.resource)
+        image = assure_rgba(image)
+        image = resize(image, *self.target_size)
+        self._image = image
+        return image
+
+
+@attrs.define(kw_only=True)
+class SingleSprite(ImageLoader):
+    joints: Joints
+
+    @property
+    def url(self) -> str | None:
+        if isinstance(self.resource, HttpResource):
+            return self.resource.url
+
+        return None
+
+
+@attrs.define(kw_only=True)
+class SheetSprite:
+    sheet: ImageLoader
+    boundary: tuple[int, int, int, int]
+    joints: Joints
+    target_size: tuple[int, int] = (0, 0)
+    _image: Image.Image | None = attrs.field(default=None, init=False)
+
+    @property
+    def url(self) -> None:
+        return None
+
+    async def load(self) -> Image.Image:
+        if self._image is not None:
+            await anyio.lowlevel.checkpoint()
+            return self._image
+
+        sprite_sheet = await self.sheet.load()
+        image = sprite_sheet.crop(self.boundary)
+        image = assure_rgba(image)
+        image = resize(image, *self.target_size)
+        self._image = image
+        return image
--- app/models/item_pack.py
+++ app/models/item_pack.py
@@ -1,4 +1,81 @@
-from supermechs.api import ItemPack
+import typing
+from collections import abc
+
+import attrs
+
+from class_utils import limited_repr
+
+from .graphics import AbstractSprite
+
+from supermechs.abc.item import ItemID
+from supermechs.abc.item_pack import PackKey
+from supermechs.api import Item, ItemData
+from supermechs.enums.stats import Tier
 
 __all__ = ("ItemPack",)
-# TODO: move over ItemPack from SM lib
+
+SpriteKey: typing.TypeAlias = tuple[ItemID, Tier]
+
+
+@attrs.define(kw_only=True)
+class ItemPack:
+    """Mapping-like container of items and their graphics."""
+
+    key: typing.Final[PackKey] = attrs.field()
+    name: str = attrs.field(default="<no name>")
+    description: str = attrs.field(default="<no description>")
+
+    items: typing.Final[abc.Mapping[ItemID, ItemData]] = attrs.field(repr=limited_repr.repr)
+    sprites: typing.Final[abc.Mapping[SpriteKey, AbstractSprite]] = attrs.field(
+        repr=limited_repr.repr
+    )
+
+    def __contains__(self, value: ItemID | ItemData, /) -> bool:
+        if isinstance(value, int):
+            return value in self.items
+
+        if isinstance(value, ItemData):
+            return value.pack_key == self.key and value.id in self.items
+
+        return False
+
+    def get_item(self, item_id: ItemID, /) -> ItemData:
+        """Lookup an item by its ID.
+
+        Raises
+        ------
+        IDLookupError: item not found.
+        """
+        return self.items[item_id]
+
+    @typing.overload
+    def get_sprite(self, item: Item, /) -> AbstractSprite: ...
+
+    @typing.overload
+    def get_sprite(self, item: ItemData, /, tier: Tier) -> AbstractSprite: ...
+
+    def get_sprite(self, item: ItemData | Item, /, tier: Tier | None = None) -> AbstractSprite:
+        """Lookup item's sprite.
+
+        Raises
+        ------
+        PackKeyError: item comes from different pack.
+        """
+        if isinstance(item, ItemData):
+            if tier is None:
+                msg = "Tier not provided with ItemData"
+                raise TypeError(msg)
+
+        elif tier is not None:
+            msg = "Tier provided for Item"
+            raise TypeError(msg)
+
+        else:
+            tier = item.tier
+            item = item.data
+
+        if item.pack_key != self.key:
+            msg = f"Mismatched pack key: {item.pack_key} != {self.key}"
+            raise ValueError(msg)
+
+        return self.sprites[item.id, tier]
