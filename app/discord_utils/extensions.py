cbec2114882460d9b940aa9466c72372c76521c7
Eneg <42005170+Enegg@users.noreply.github.com>
2024-07-26 21:39:48 +0200

    Add `exceptiongroup` as conditional dependency

--- app/discord_utils/extensions.py
+++ app/discord_utils/extensions.py
@@ -1,11 +1,13 @@
 """
 Utilities related to handling bot extensions.
 """
+import sys
 from collections import abc
 
-from exceptiongroup import ExceptionGroup
-
 from .pending import find_submodules, walk_modules
+
+if sys.version_info < (3, 11):
+    from exceptiongroup import ExceptionGroup
 
 __all__ = ("load_extensions",)
 
