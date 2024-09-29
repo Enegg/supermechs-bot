de5a576a4f1c8996daf13da9dcc4828880a0ea30
Eneg <42005170+Enegg@users.noreply.github.com>
2024-09-29 03:42:16 +0200

    Fuzzy item matching

--- src/app/bridges/autocompleters.py
+++ src/app/bridges/autocompleters.py
@@ -1,27 +1,106 @@
-from collections import abc, defaultdict
-from typing import Any
+from collections import abc
+from difflib import SequenceMatcher
+from typing import Any, NamedTuple
 
 from discord import AutocompleteReturnType, InteractionLimits
 from disnake import CommandInteraction
 
 from app.bridges.user_input import StringLimits
 from app.shared.item_packs import get_item_pack_for
-from app.sm.name_utils import acronym_of, search_for
 from app.stored import players
 
-from supermechs.abc.item import Name
 from supermechs.api import Element, ItemData, Type
 
 __all__ = ("item_name_autocomplete", "mech_name_autocomplete")
 
-acronyms: abc.Mapping[str, set[Name]] = defaultdict(set)
-item_names: dict[str, ItemData] = {}
+
+class MatchResult(NamedTuple):
+    name: str
+    direct_score: float
+    multiword_scores: abc.Sequence[float]
+    is_acronym: bool
+
+    def sort_key(self) -> tuple[object, ...]:
+        return (
+            self.is_acronym,
+            max(self.direct_score, max(self.multiword_scores, default=0.0)),
+            self.direct_score,
+        )
 
 
-def _make_acronyms(names: abc.Iterable[Name], /) -> None:
+def get_ratio(name: str, matcher: SequenceMatcher[str], cutoff: float = 0.3) -> float:
+    matcher.set_seq1(name)
+
+    if (score := matcher.real_quick_ratio()) < cutoff:
+        return score
+
+    if (score := matcher.quick_ratio()) < cutoff:
+        return score
+
+    return matcher.ratio()
+
+
+def get_multiword_scores(
+    name: str, phrase_parts: abc.Sequence[str], matcher: SequenceMatcher[str]
+) -> abc.Sequence[float]:
+    name_parts = name.split(" ")
+
+    if len(name_parts) == 1 or len(name_parts) < len(phrase_parts):
+        return ()
+
+    scores: list[float] = []
+
+    for phrase_part in phrase_parts:
+        matcher.set_seq2(phrase_part)
+
+        max_score: float = 0.0
+
+        for name_part in name_parts:
+            max_score = max(get_ratio(name_part, matcher), max_score)
+
+        scores.append(max_score)
+
+    return scores
+
+
+def acronym_of(name: str, /) -> str | None:
+    """Return an acronym of the name, or None if one cannot (shouldn't) be made.
+
+    The acronym consists of capital letters in item's name;
+    it will not be made for non-PascalCase single-word names, or names which themselves
+    are an acronym for something (like EMP).
+    """
+    if not name:
+        return None
+    if name[0].isupper() and name[1:].islower():
+        # don't bother with single capital letters
+        return None
+    # filter out already-acronym names, like "EMP"
+    if name.isupper():
+        return None
+    # names which are partially acronyms are fine
+    return "".join(filter(str.isupper, name)).lower()
+
+
+def find_matches(names: abc.Iterable[str], phrase: str) -> list[MatchResult]:
+    phrase = phrase.lower()
+    phrase_parts = [part for raw_part in phrase.split(" ") if (part := raw_part.strip())]
+
+    whole_matcher = SequenceMatcher(b=phrase)
+    multiword_matcher = SequenceMatcher[str]()
+
+    results: list[MatchResult] = []
+
     for name in names:
-        if acronym := acronym_of(name):
-            acronyms[acronym].add(name)
+        lowercase_name = name.lower()
+        direct_score = get_ratio(lowercase_name, whole_matcher)
+        multiword_scores = get_multiword_scores(lowercase_name, phrase_parts, multiword_matcher)
+        is_acronym = acronym_of(name) == phrase
+
+        results.append(MatchResult(name, direct_score, multiword_scores, is_acronym))
+
+    results.sort(key=MatchResult.sort_key, reverse=True)
+    return results
 
 
 def _get_item_filters(options: abc.Mapping[str, Any], /) -> list[abc.Callable[[ItemData], bool]]:
@@ -43,42 +122,15 @@ async def item_name_autocomplete(inter: "CommandInteraction", input: str) -> Aut
     pack = get_item_pack_for(inter)
     filters = _get_item_filters(inter.filled_options)
 
-    if not item_names:
-        for item in pack.items.values():
-            item_names[item.name] = item
-
-        _make_acronyms(item_names)
-
-    def filter_item_names(names: abc.Iterable[Name], /) -> abc.Iterator[Name]:
-        items = map(item_names.__getitem__, names)
+    def filter_item_names() -> abc.Iterator[str]:
+        items = (cont.item for cont in pack.items.values())
         items = (item for item in items if all(func(item) for func in filters))
         return (item.name for item in items)
 
     input = input.strip()
-
-    # place matching abbreviations at the top
-    if items := acronyms.get(input.lower()):
-        matching_item_names = sorted(filter_item_names(items) if filters else items)
-
-        # this shouldn't ever happen, but handle it anyway
-        if len(matching_item_names) >= InteractionLimits.autocomplete_options:
-            del matching_item_names[InteractionLimits.autocomplete_options :]
-            return matching_item_names
-
-        # extra filter to exclude duplicates
-        filters.append(lambda item: item.name not in items)
-
-    else:
-        matching_item_names = []
-
-    import heapq
-
-    # extend names up to option limit
-    matching_item_names += heapq.nsmallest(
-        InteractionLimits.autocomplete_options - len(matching_item_names),
-        filter_item_names(search_for(input, item_names)),
-    )
-    return matching_item_names
+    matching = find_matches(filter_item_names(), input)
+    del matching[InteractionLimits.autocomplete_options :]
+    return [result.name for result in matching]
 
 
 async def mech_name_autocomplete(inter: CommandInteraction, input: str) -> AutocompleteReturnType:
