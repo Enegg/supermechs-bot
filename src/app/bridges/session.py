b9c198accfe045630bc7bebac414671add966653
Eneg <42005170+Enegg@users.noreply.github.com>
2024-10-02 00:10:15 +0200

    Log requests

--- src/app/bridges/session.py
+++ src/app/bridges/session.py
@@ -1,18 +1,21 @@
-from functools import partial
-from typing import Protocol
+import logging
+from typing import Any, Protocol
 
 import aiohttp
 import orjson
+from aiohttp.typedefs import StrOrURL
 
 __all__ = ("client_session",)
 
+_LOG = logging.getLogger(__name__)
 
-class HTTPClient(Protocol):
+
+class HttpClient(Protocol):
     connector: aiohttp.BaseConnector | None
     proxy: str | None
 
 
-def client_session(client: HTTPClient, /) -> aiohttp.ClientSession:
+def client_session(client: HttpClient, /) -> aiohttp.ClientSession:
     """Create a client session with client's connector & proxy."""
 
     def _dumps(obj: object, /) -> str:
@@ -20,8 +23,15 @@ def client_session(client: HTTPClient, /) -> aiohttp.ClientSession:
 
     session = aiohttp.ClientSession(
         connector=client.connector,
+        connector_owner=client.connector is None,
         timeout=aiohttp.ClientTimeout(total=30),
         json_serialize=_dumps,
     )
-    session._request = partial(session._request, proxy=client.proxy)  # pyright: ignore[reportPrivateUsage]
+    _request = session._request  # pyright: ignore[reportPrivateUsage]
+
+    def _log_request(method: str, str_or_url: StrOrURL, **kwargs: Any) -> Any:  # noqa: ANN401
+        _LOG.info("method=%s url=%s", method, str_or_url)
+        return _request(method, str_or_url, proxy=client.proxy, **kwargs)
+
+    session._request = _log_request  # pyright: ignore[reportPrivateUsage]
     return session
