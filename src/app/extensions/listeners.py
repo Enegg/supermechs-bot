aa06cef4a352d493ab46209f7559c99fa67b15a5
Eneg <42005170+Enegg@users.noreply.github.com>
2024-11-05 00:23:53 +0100

    Log more relevant data

--- src/app/extensions/listeners.py
+++ src/app/extensions/listeners.py
@@ -9,26 +9,34 @@ from app.bridges.cancellation import is_cancel_button, parse_id
 from app.core import CONFIG
 
 plugin = Plugin(name="listeners", logger="ext")
-_EVENTS_LOG = logging.getLogger("event")
+_LOG = logging.getLogger("event")
 
 
 @plugin.listener(Event.ready)
 async def on_ready() -> None:
-    _EVENTS_LOG.info("%s is ready", plugin.bot.user.name)
-
     if __debug__:
         limit = plugin.bot.session_start_limit
         assert limit is not None
-        _EVENTS_LOG.info(
-            f"Session #{limit.total - limit.remaining}/{limit.total}"
+        _LOG.info(
+            f"Username: {plugin.bot.user.name};"
+            f" Session #{limit.total - limit.remaining}/{limit.total}"
             f" (expires {limit.reset_time:{CONFIG.date_format}})"
         )
+
+    else:
+        _LOG.info(f"Username: {plugin.bot.user.name}")
 
 
 @plugin.listener(Event.slash_command)
 async def on_slash_command(inter: CommandInteraction, /) -> None:
     command_name = inter.application_command.qualified_name
-    _EVENTS_LOG.debug("Slash command invoked: /%s", command_name)
+    _LOG.info(
+        "%s (%d): /%s",
+        inter.author,
+        inter.author.id,
+        command_name,
+        extra={"filled_options": inter.filled_options},
+    )
     add_invocation(inter.data.id, command_name)
 
 
@@ -40,6 +48,13 @@ async def on_cancel_button(inter: ui.MessageInteraction, /) -> None:
     await inter.response.defer()
     await inter.delete_original_response()
     cancel_for(parse_id(inter.data.custom_id))
+
+
+@plugin.listener(Event.slash_command_completion)
+async def on_slash_command_completion(inter: CommandInteraction, /) -> None:
+    command_name = inter.application_command.qualified_name
+    result = "failed" if inter.command_failed else "finished"
+    _LOG.info("Command by %s %s: /%s", inter.author, result, command_name)
 
 
 setup, teardown = plugin.create_extension_handlers()
