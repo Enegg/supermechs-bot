f9a9efb5e89940383a4945c2e17ff5be45132b21
Eneg <42005170+Enegg@users.noreply.github.com>
2024-12-11 05:29:24 +0100

    Simplify addon stat discovery

--- src/smparse/packs/structuring.py
+++ src/smparse/packs/structuring.py
@@ -1,7 +1,7 @@
 import math
 from collections import abc
 from enum import Enum
-from typing import Any, NamedTuple, SupportsFloat
+from typing import Any, SupportsFloat, SupportsIndex, TypeAlias
 
 from smparse.converter import converter
 from smparse.packs.models import AnyItemPack, ItemPackV1, ItemPackV2, ItemPackV3
@@ -13,53 +13,52 @@ class Sentinel(Enum):
     instance = None
 
 
-class _NameAndAddon(NamedTuple):
-    name: str
-    has_addon: bool = False
-
-
-_STAT_NAME_TABLE: abc.Mapping[str, _NameAndAddon] = {
-    "weight": _NameAndAddon("weight"),
-    "health": _NameAndAddon("hit_points"),
-    "eneCap": _NameAndAddon("energy_capacity"),
-    "eneReg": _NameAndAddon("regeneration"),
-    "heaCap": _NameAndAddon("heat_capacity"),
-    "heaCol": _NameAndAddon("cooling"),
-    "phyRes": _NameAndAddon("physical_resistance"),
-    "expRes": _NameAndAddon("explosive_resistance"),
-    "eleRes": _NameAndAddon("electric_resistance"),
-    "bulletsCap": _NameAndAddon("bullets_capacity"),
-    "rocketsCap": _NameAndAddon("rockets_capacity"),
-    "walk": _NameAndAddon("walk"),
-    "jump": _NameAndAddon("jump"),
-    "phyDmg": _NameAndAddon("physical_damage", True),
-    "phyResDmg": _NameAndAddon("physical_resistance_damage"),
-    "eleDmg": _NameAndAddon("electric_damage", True),
-    "eneDmg": _NameAndAddon("energy_damage"),
-    "eneCapDmg": _NameAndAddon("energy_capacity_damage"),
-    "eneRegDmg": _NameAndAddon("regeneration_damage"),
-    "eleResDmg": _NameAndAddon("electric_resistance_damage"),
-    "expDmg": _NameAndAddon("explosive_damage", True),
-    "heaDmg": _NameAndAddon("heat_damage"),
-    "heaCapDmg": _NameAndAddon("heat_capacity_damage"),
-    "heaColDmg": _NameAndAddon("cooling_damage"),
-    "expResDmg": _NameAndAddon("explosive_resistance_damage"),
-    "range": _NameAndAddon("range", True),
-    "push": _NameAndAddon("push"),
-    "pull": _NameAndAddon("pull"),
-    "recoil": _NameAndAddon("recoil"),
-    "advance": _NameAndAddon("advance"),
-    "retreat": _NameAndAddon("retreat"),
-    "uses": _NameAndAddon("uses"),
-    "backfire": _NameAndAddon("backfire"),
-    "heaCost": _NameAndAddon("heat_generation"),
-    "eneCost": _NameAndAddon("energy_cost"),
-    "bulletsCost": _NameAndAddon("bullets_cost"),
-    "rocketsCost": _NameAndAddon("rockets_cost"),
+_STAT_RENAME_TABLE: abc.Mapping[str, str] = {
+    "weight": "weight",
+    "health": "hit_points",
+    "eneCap": "energy_capacity",
+    "eneReg": "regeneration",
+    "heaCap": "heat_capacity",
+    "heaCol": "cooling",
+    "phyRes": "physical_resistance",
+    "expRes": "explosive_resistance",
+    "eleRes": "electric_resistance",
+    "bulletsCap": "bullets_capacity",
+    "rocketsCap": "rockets_capacity",
+    "walk": "walk",
+    "jump": "jump",
+    "phyDmg": "physical_damage",
+    "phyResDmg": "physical_resistance_damage",
+    "eleDmg": "electric_damage",
+    "eneDmg": "energy_damage",
+    "eneCapDmg": "energy_capacity_damage",
+    "eneRegDmg": "regeneration_damage",
+    "eleResDmg": "electric_resistance_damage",
+    "expDmg": "explosive_damage",
+    "heaDmg": "heat_damage",
+    "heaCapDmg": "heat_capacity_damage",
+    "heaColDmg": "cooling_damage",
+    "expResDmg": "explosive_resistance_damage",
+    "range": "range",
+    "push": "push",
+    "pull": "pull",
+    "recoil": "recoil",
+    "advance": "advance",
+    "retreat": "retreat",
+    "uses": "uses",
+    "backfire": "backfire",
+    "heaCost": "heat_generation",
+    "eneCost": "energy_cost",
+    "bulletsCost": "bullets_cost",
+    "rocketsCost": "rockets_cost",
 }
+_STAT_WITH_ADDON: abc.Set[str] = {"phyDmg", "eleDmg", "expDmg", "range"}
 
 
-def none_to_nan(value: SupportsFloat | None, /) -> float:
+ConvertibleToFloat: TypeAlias = str | SupportsFloat | SupportsIndex
+
+
+def none_to_nan(value: ConvertibleToFloat | None, /) -> float:
     return math.nan if value is None else float(value)
 
 
@@ -68,7 +67,8 @@ def structure_stats(obj: abc.Mapping[str, Any | None | list[Any | None]], _: typ
     stats_mapping: dict[str, float] = {}
     problems: list[Exception] = []
 
-    for key, (name, has_addon) in _STAT_NAME_TABLE.items():
+    for key, name in _STAT_RENAME_TABLE.items():
+        has_addon = key in _STAT_WITH_ADDON
         try:
             match obj.get(key, Sentinel.instance):
                 case Sentinel.instance:
@@ -90,7 +90,7 @@ def structure_stats(obj: abc.Mapping[str, Any | None | list[Any | None]], _: typ
                     stats_mapping[name] = none_to_nan(value)
 
         # float conversion
-        except (TypeError, ValueError) as err:
+        except (TypeError, ValueError, OverflowError) as err:
             problems.append(err)
 
     if problems:
