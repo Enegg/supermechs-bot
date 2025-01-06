9236459a0aa16630806e1d9278e43369cdf167d3
Eneg <42005170+Enegg@users.noreply.github.com>
2025-01-06 04:03:07 +0100

    Refactor injections

--- src/app/bridges/injectors.py
+++ src/app/bridges/injectors.py
@@ -14,43 +14,45 @@ from supermechs.all import ItemData
 __all__ = ("register_injections",)
 
 
+def _inject_locale(inter: CommandInteraction, /) -> disnake.Locale:
+    return i18n.locale_override.unwrap_or(inter.locale)
+
+
+def _inject_item(inter: CommandInteraction, /, name: str) -> ItemData:
+    """Injection taking Item name and returning ItemData.
+
+    Parameters
+    ----------
+    name: The name of the item. {{ ITEM_NAME }}
+    """
+    item_pack = get_item_pack_for(inter)
+    item = get_item_by_name(item_pack.items.values(), name=name)
+    if item is not None:
+        return item
+
+    locale = _inject_locale(inter)
+    msg = i18n.get_message(locale, "unknown-item-name", name=name)
+    raise commands.UserInputError(msg)
+
+
+def _inject_gettext(inter: CommandInteraction, /) -> i18n.GetText:
+    return i18n.get_gettext(_inject_locale(inter))
+
+
+def _inject_player(inter: CommandInteraction, /) -> Player:
+    return players(inter.author)
+
+
 def register_injections() -> None:
     """Entry point for registering all injections for the commands module."""
     # NOTE: this function exists purely so as not to have the injectors
     # being registered as a *side effect* of importing this module (as otherwise
     # somewhere in the main.py we'd need a blank import which isn't used anywhere)
 
-    @commands.register_injection
-    def inject_item(inter: CommandInteraction, name: str) -> ItemData:
-        """Injection taking Item name and returning ItemData.
-
-        Parameters
-        ----------
-        name: The name of the item. {{ ITEM_NAME }}
-        """
-        item_pack = get_item_pack_for(inter)
-        item = get_item_by_name(item_pack.items.values(), name=name)
-        if item is not None:
-            return item
-
-        locale = inject_locale(inter)
-        msg = i18n.get_message(locale, "unknown-item-name", name=name)
-        raise commands.UserInputError(msg)
-
-    @commands.register_injection
-    def inject_player(inter: CommandInteraction) -> Player:
-        """Injection creating a player from interaction."""
-        return players(inter.author)
-
-    @commands.register_injection
-    def inject_locale(inter: CommandInteraction) -> disnake.Locale:
-        """Injection returning context aware locale."""
-        return i18n.locale_override.unwrap_or(inter.locale)
-
-    @commands.register_injection
-    def inject_gettext(inter: CommandInteraction) -> i18n.GetText:
-        """Injection returning a callable which returns localized messages."""
-        return i18n.get_gettext(inject_locale(inter))
-
-    inject_item.autocomplete("name")(item_name_autocomplete)
-    del inject_player, inject_gettext
+    commands.register_injection(get_item_pack_for)
+    commands.register_injection(_inject_locale)
+    commands.register_injection(_inject_gettext)
+    commands.register_injection(_inject_player)
+    commands.register_injection(
+        _inject_item,
+    ).autocomplete("name")(item_name_autocomplete)
