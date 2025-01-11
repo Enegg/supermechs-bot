59c0e678f6f82ebed70e805ec7f9dae5a0629aa1
Enegg <42005170+Enegg@users.noreply.github.com>
2025-01-11 16:38:23 +0100

    Yeet session from state

--- src/app/state.py
+++ src/app/state.py
@@ -1,23 +1,12 @@
-import asyncio
 from typing import Final
-from typing_extensions import override
 
-import aiohttp
 import attrs
 
 from app.models.item_pack import ItemPack, PackKey
 
 
-class _NullEventLoop(asyncio.AbstractEventLoop):
-    @override
-    def get_debug(self) -> bool:
-        return False
-
-
 @attrs.define
 class State:
-    # perhaps turn it into a property
-    http_session: aiohttp.ClientSession = aiohttp.ClientSession(loop=_NullEventLoop())
     item_pack: ItemPack = ItemPack(key=PackKey("$not-loaded"))
 
 
