0005f0bda5d31e7262c054f8a8a3f42663f8a4db
Eneg <42005170+Enegg@users.noreply.github.com>
2025-06-23 00:30:10 +0200

    Remove broken compare command for now

--- src/app/extensions/item_lookup/__init__.py
+++ src/app/extensions/item_lookup/__init__.py
@@ -1,9 +1,8 @@
-import io
 from collections import abc
 from typing import NamedTuple
 
 from app.disnake_types import CommandInteraction
-from discord import ComponentLimits, MessageLimits
+from discord import ComponentLimits
 from disnake import Embed, Event, Locale
 from disnake.ext import commands
 
@@ -11,12 +10,10 @@ from app import i18n, ui
 from app.assets import COLORS, EMOJIS, get_slot_icon
 from app.commands.autocompleters import item_name_autocomplete
 from app.commands.params import ELEMENT_CHOICES, TIER_CHOICES, TYPE_CHOICES
-from app.core import CONFIG
 from app.devtools import debug_message
 from app.gamerules import MAXED_ARENA_BUFFS
 from app.managers import gfx, packs
 from app.plugins_factory import create_plugin
-from defer import Defer
 from resources import HttpResource
 
 from .helpers import (
@@ -570,121 +567,6 @@ async def on_item_lookup_interaction(inter: ui.MessageInteraction) -> None:
     if __debug__:
         debug_message(embed, layout)
     await inter.response.edit_message(embed=embed, components=layout)
-
-
-@plugin.slash_command(guild_ids=CONFIG.test_guild_ids)
-async def item_raw(
-    inter: CommandInteraction,
-    name: str = commands.Param(autocomplete=item_name_autocomplete),
-    type: str | None = commands.Param(None, choices=TYPE_CHOICES),
-    element: str | None = commands.Param(None, choices=ELEMENT_CHOICES),
-    rarity: str | None = commands.Param(None, choices=TIER_CHOICES),
-    legacy: bool = False,
-) -> None:
-    """Lookup raw item stats.
-
-    Parameters
-    ----------
-    name:
-        The name of the item.
-    type:
-        Limit suggestions to this type.
-    element:
-        Limit suggestions to this element.
-    rarity:
-        Remove suggestions below this rarity.
-    legacy:
-        Show legacy items.
-    """
-    for item in packs.filter_items(type, element, rarity, legacy):
-        if item.name == name:
-            break
-
-    else:
-        msg = i18n.get_message(i18n.get_locale(inter), "unknown-item-name", name=name)
-        raise commands.UserInputError(msg)
-
-    await inter.response.send_message(f"`{item!r:.{MessageLimits.content - 2}}`", ephemeral=True)
-
-
-def str_type(type: sm.Item.Type) -> str:
-    return type.name.replace("_", " ").lower()
-
-
-def str_elem(element: sm.Item.Element) -> str:
-    return element.name.capitalize()
-
-
-@plugin.slash_command()
-async def compare(
-    inter: CommandInteraction,
-    item_a_name: str = commands.Param(name="item1", autocomplete=item_name_autocomplete),
-    item_b_name: str = commands.Param(name="item2", autocomplete=item_name_autocomplete),
-) -> None:
-    """Interactive comparison between two items. {{ COMPARE }}
-
-    Parameters
-    ----------
-    item_a_name:
-        First item to compare. {{ COMPARE_FIRST }}
-    item_b_name:
-        Second item to compare. {{ COMPARE_SECOND }}
-    """  # noqa: D400
-    locale = i18n.get_locale(inter)
-    item_a = None
-    item_b = None
-
-    match item_a, item_b:
-        case None, None:
-            msg = "Items not found."
-            raise commands.UserInputError(msg)
-        case None, _:  # pyright: ignore[reportUnnecessaryComparison]
-            msg = "Item1 not found."
-            raise commands.UserInputError(msg)
-        case _, None:  # pyright: ignore[reportUnnecessaryComparison]
-            msg = "Item2 not found."
-            raise commands.UserInputError(msg)
-        case _:
-            pass
-
-    if item_a.element is item_b.element:
-        desc_builder = io.StringIO()
-        desc_builder.write(str_elem(item_a.element))
-
-        if item_a.type is item_b.type:
-            desc_builder.write(" ")
-            type_ = str_type(item_a.type)
-            desc_builder.write(type_)
-
-            if not type_.endswith("s"):  # legs do end with s
-                desc_builder.write("s")
-
-        else:
-            desc_builder.write(f" {str_type(item_a.type)} / {str_type(item_b.type)}")
-
-        desc = desc_builder.getvalue()
-        color = COLORS.elements[item_a.element]
-
-    else:
-        desc = (
-            f"{str_elem(item_a.element)} {str_type(item_a.type)}"
-            f" | {str_elem(item_b.element)} {str_type(item_b.type)}"
-        )
-        color = inter.author.color
-
-    embed = Embed(title=f"{item_a.name} vs {item_b.name}", description=desc, color=color)
-
-    await inter.response.send_message(embed=embed, ephemeral=True)
-    return  # FIXME
-
-    from .item_lookup import item_compare_view  # noqa: PLC0415
-
-    store = ui.callback_store(inter)
-    layout = item_compare_view(store, embed, item_a, item_b, locale)
-    await inter.response.send_message(embed=embed, components=layout, ephemeral=True)
-    async with Defer(shield=True) as defer:
-        defer(inter.edit_original_response, components=None)
-        await store.listen(timeout=CONFIG.user_input_timeout)
 
 
 setup, teardown = plugin.create_extension_handlers()
