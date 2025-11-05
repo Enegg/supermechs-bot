e8b105e1a90c980eccc7c75f37676dc26f6f5fba
Eneg <42005170+Enegg@users.noreply.github.com>
2025-11-05 05:28:43 +0100

    remove unused helpers

--- src/app/extensions/item_lookup/helpers.py
+++ src/app/extensions/item_lookup/helpers.py
@@ -5,7 +5,6 @@ from disnake import Locale
 
 from app import i18n
 from app.assets import EMOJIS
-from app.text_utils import Char, acronym_of
 
 import dupermechs.all as sm
 from dupermechs.enums import ItemStat
@@ -23,16 +22,6 @@ def format_float(num: float, decimals: int) -> str:
     return f"{num:.{decimals}f}"
 
 
-def try_shorten(name: str, limit: int = 16) -> str:
-    if len(name) <= limit:
-        return name
-
-    if (acronym := acronym_of(name)) is not None and len(acronym) <= limit:
-        return acronym
-
-    return name[: limit - 1] + Char.TRIPLE_DOT
-
-
 def format_damage_default(lo: int, hi: int, /) -> str:
     if hi and lo != hi:
         return f"{lo}-{hi}"
@@ -40,15 +29,9 @@ def format_damage_default(lo: int, hi: int, /) -> str:
     return str(lo)
 
 
-def mean_and_deviation(a: int | float, b: int | float, /) -> tuple[float, float]:
-    mean = (a + b) / 2
-    deviation = math.sqrt(((a - mean) ** 2 + (b - mean) ** 2) / 2)
-    return mean, deviation
-
-
 def format_damage_average(lo: int | float, hi: int | float, /, *, decimals: int = 1) -> str:
-    mean, deviation = mean_and_deviation(lo, hi)
-    dev = deviation / mean * 100.0
+    mean = (lo + hi) / 2
+    dev = math.sqrt(((lo - mean) ** 2 + (hi - mean) ** 2) / 2) / mean * 100.0
     str_mean = format_float(mean, 1)
     str_dev = format_float(dev, decimals)
     return f"{str_mean} ±{str_dev}%"
