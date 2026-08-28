79358160ae9c874138732cc1e329a5c8e0d1578c
Eneg <42005170+Enegg@users.noreply.github.com>
2026-08-28 23:39:05 +0200

    Fix indent

--- src/app/extensions/item_lookup/reloaded_lookup.py
+++ src/app/extensions/item_lookup/reloaded_lookup.py
@@ -350,7 +350,7 @@ def get_item_summary(gettext: i18n.GetText, item: sm.Item, ctx: UIContext) -> ui
     if item.subtype is sm.Item.Subtype.power_kit:
         boost_power = levels[ctx.level_index].power_contribution
         add_component(ui.TextDisplay(
-                f"{EMOJIS.stat_power} **{boost_power}** {gettext('boost-power')}"
+            f"{EMOJIS.stat_power} **{boost_power}** {gettext('boost-power')}"
         ))  # fmt: skip
     else:
         stats_lines, costs_lines = format_stats(item, item_stats, gettext, avg=ctx.damage_average)
