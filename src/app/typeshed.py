251432fbc8fb85a9f6e802324a9c3e30a65bc900
Eneg <42005170+Enegg@users.noreply.github.com>
2026-08-29 13:46:01 +0200

    wip

--- src/app/typeshed.py
+++ src/app/typeshed.py
@@ -1,5 +1,9 @@
 import os
+import typing
 from collections import abc
+
+if typing.TYPE_CHECKING:
+    import _typeshed
 
 type Factory[RetT] = abc.Callable[[], RetT]
 """0-argument callable returning an object of given type."""
@@ -7,3 +11,17 @@ type Pathish = os.PathLike[str] | str
 """Path-like or a string representing a path."""
 type AsyncFunc[**P, RetT] = abc.Callable[P, abc.Awaitable[RetT]]
 """Function yielding an awaitable."""
+
+type ConvertibleToInt = _typeshed.ConvertibleToInt
+"""Value acceptable as an argument to `int()`."""
+type ConvertibleToFloat = _typeshed.ConvertibleToFloat
+"""Value acceptable as an argument to `float()`."""
+
+type SupportsLt[T] = _typeshed.SupportsDunderLT[T]
+"""Type implementing `__lt__`."""
+type SupportsGt[T] = _typeshed.SupportsDunderGT[T]
+"""Type implementing `__gt__`."""
+type SupportsLe[T] = _typeshed.SupportsDunderLE[T]
+"""Type implementing `__le__`."""
+type SupportsGe[T] = _typeshed.SupportsDunderGE[T]
+"""Type implementing `__ge__`."""
